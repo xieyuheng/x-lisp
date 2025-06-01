da8e21226719a1ccbf483719c18a09e610d371b9
Xie Yuheng <xyheme@gmail.com>
2025-06-01 09:31:50 +0800

    up

--- examples/essentials-of-compilation/lang-c-var.lisp
+++ examples/essentials-of-compilation/lang-c-var.lisp
@@ -1,20 +1,3 @@
-(define-datatype (list-t A)
-  [list-null () (list-t A)]
-  [list-cons ([head A] [tail (list-t A)]) (list-t A)])
-
-;; var
-
-(define-datatype exp-t
-  [exp-var ([name string-t]) exp-t]
-  [exp-int ([value int-t]) exp-t]
-  [exp-prim ([op string-t] [args (list-t exp-t)]) exp-t]
-  [exp-let ([name string-t] [rhs exp-t] [body exp-t]) exp-t])
-
-(define-datatype program-t
-  [program-new ([info info-t] [body exp-t]) program-t])
-
-;; c-var
-
 (define-datatype atom-t
   [atom-var ([name string-t]) atom-t]
   [atom-int ([value int-t]) atom-t])
