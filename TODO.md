585021709719babfcad53eaa23b0673d7ab93199
Xie Yuheng <xyheme@gmail.com>
2025-07-31 18:33:28 +0800

    up

--- TODO.md
+++ TODO.md
@@ -1,1 +1,2 @@
+remove Neutral
 add builtin mechanism
--- src/lang/load/handleEffect.ts
+++ src/lang/load/handleEffect.ts
@@ -1,45 +1,10 @@
 import { emptyEnv } from "../env/index.ts"
-import { equal } from "../equal/index.ts"
 import { evaluate } from "../evaluate/index.ts"
-import { formatExp, formatValue } from "../format/index.ts"
+import { formatValue } from "../format/index.ts"
 import type { Mod } from "../mod/index.ts"
 import type { Stmt } from "../stmt/index.ts"
 
 export async function handleEffect(mod: Mod, stmt: Stmt): Promise<void> {
-  if (stmt.kind === "AssertEqual") {
-    if (
-      equal(
-        evaluate(mod, emptyEnv(), stmt.lhs),
-        evaluate(mod, emptyEnv(), stmt.rhs),
-      )
-    ) {
-      return
-    }
-
-    throw new Error(
-      `[assert-equal] fail:\n` +
-        `  lhs: ${formatExp(stmt.lhs)}\n` +
-        `  rhs: ${formatExp(stmt.rhs)}\n`,
-    )
-  }
-
-  if (stmt.kind === "AssertNotEqual") {
-    if (
-      !equal(
-        evaluate(mod, emptyEnv(), stmt.lhs),
-        evaluate(mod, emptyEnv(), stmt.rhs),
-      )
-    ) {
-      return
-    }
-
-    throw new Error(
-      `[assert-not-equal] fail:\n` +
-        `  lhs: ${formatExp(stmt.lhs)}\n` +
-        `  rhs: ${formatExp(stmt.rhs)}\n`,
-    )
-  }
-
   if (stmt.kind === "Compute") {
     const value = evaluate(mod, emptyEnv(), stmt.exp)
     console.log(formatValue(value))
