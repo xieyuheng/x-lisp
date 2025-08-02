9d1c8f973474094726b592739749a030b1ae6d9d
Xie Yuheng <xyheme@gmail.com>
2025-08-02 20:52:31 +0800

    Stmt has meta.span

--- src/lang/parse/matchStmt.ts
+++ src/lang/parse/matchStmt.ts
@@ -5,29 +5,38 @@ import { type Stmt } from "../stmt/index.ts"
 import { matchExp } from "./matchExp.ts"
 
 const stmtMatcher: X.Matcher<Stmt> = X.matcherChoice<Stmt>([
-  X.matcher("`(define ,(cons name args) ,exp)", ({ name, args, exp }) =>
-    Stmts.Define(
-      X.symbolToString(name),
-      X.dataToArray(args)
-        .map(X.symbolToString)
-        .reduceRight((fn, name) => Exps.Lambda(name, fn), matchExp(exp)),
-    ),
+  X.matcher(
+    "`(define ,(cons name args) ,exp)",
+    ({ name, args, exp }, { span }) =>
+      Stmts.Define(
+        X.symbolToString(name),
+        X.dataToArray(args)
+          .map(X.symbolToString)
+          .reduceRight((fn, name) => Exps.Lambda(name, fn), matchExp(exp)),
+        { span },
+      ),
   ),
 
-  X.matcher("`(define ,name ,exp)", ({ name, exp }) =>
-    Stmts.Define(X.symbolToString(name), matchExp(exp)),
+  X.matcher("`(define ,name ,exp)", ({ name, exp }, { span }) =>
+    Stmts.Define(X.symbolToString(name), matchExp(exp), { span }),
   ),
 
-  X.matcher("(cons 'import body)", ({ body }) => {
+  X.matcher("(cons 'import body)", ({ body }, { span }) => {
     const array = X.dataToArray(body)
     const url = array[array.length - 1]
     const entries = array.slice(0, array.length - 1)
-    return Stmts.Import(X.dataToString(url), entries.map(matchImportEntry))
+    return Stmts.Import(X.dataToString(url), entries.map(matchImportEntry), {
+      span,
+    })
   }),
 
-  X.matcher("`(assert ,exp)", ({ exp }) => Stmts.Assert(matchExp(exp))),
+  X.matcher("`(assert ,exp)", ({ exp }, { span }) =>
+    Stmts.Assert(matchExp(exp), { span }),
+  ),
 
-  X.matcher("exp", ({ exp }) => Stmts.Compute(matchExp(exp))),
+  X.matcher("exp", ({ exp }, { span }) =>
+    Stmts.Compute(matchExp(exp), { span }),
+  ),
 ])
 
 export function matchStmt(data: X.Data): Stmt {
@@ -37,12 +46,16 @@ export function matchStmt(data: X.Data): Stmt {
 function matchImportEntry(data: X.Data): Stmts.ImportEntry {
   return X.match(
     X.matcherChoice([
-      X.matcher("`(rename ,name ,rename)", ({ name, rename }) => ({
+      X.matcher("`(rename ,name ,rename)", ({ name, rename }, { span }) => ({
         name: X.symbolToString(name),
         rename: X.symbolToString(rename),
+        meta: { span },
       })),
 
-      X.matcher("name", ({ name }) => ({ name: X.symbolToString(name) })),
+      X.matcher("name", ({ name }, { span }) => ({
+        name: X.symbolToString(name),
+        meta: { span },
+      })),
     ]),
     data,
   )
--- src/lang/stmt/Stmt.ts
+++ src/lang/stmt/Stmt.ts
@@ -1,60 +1,76 @@
+import { type Span } from "@xieyuheng/x-data.js"
 import { type Exp } from "../exp/index.ts"
+
+type Meta = { span: Span }
 
 export type Stmt = Compute | Define | Import | Assert
 
 export type Compute = {
   kind: "Compute"
   exp: Exp
+  meta: Meta
 }
 
 export type Define = {
   kind: "Define"
   name: string
   exp: Exp
+  meta: Meta
 }
 
 export type Import = {
   kind: "Import"
   path: string
   entries: Array<ImportEntry>
+  meta: Meta
 }
 
 export type ImportEntry = {
   name: string
   rename?: string
+  meta: Meta
 }
 
 export type Assert = {
   kind: "Assert"
   exp: Exp
+  meta: Meta
 }
 
-export function Compute(exp: Exp): Compute {
+export function Compute(exp: Exp, meta: Meta): Compute {
   return {
     kind: "Compute",
     exp,
+    meta,
   }
 }
 
-export function Define(name: string, exp: Exp): Define {
+export function Define(name: string, exp: Exp, meta: Meta): Define {
   return {
     kind: "Define",
     name,
     exp,
+    meta,
   }
 }
 
-export function Import(path: string, entries: Array<ImportEntry>): Import {
+export function Import(
+  path: string,
+  entries: Array<ImportEntry>,
+  meta: Meta,
+): Import {
   return {
     kind: "Import",
     path,
     entries,
+    meta,
   }
 }
 
-export function Assert(exp: Exp): Assert {
+export function Assert(exp: Exp, meta: Meta): Assert {
   return {
     kind: "Assert",
     exp,
+    meta,
   }
 }
