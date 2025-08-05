708b6296c7c8dc4c4439adf4692a90f3cda051ad
Xie Yuheng <xyheme@gmail.com>
2025-08-06 06:16:36 +0800

    up

--- TODO.md
+++ TODO.md
@@ -1,7 +1,5 @@
 # conditional
 
-`Exp` -- `Not` -- no need to be lazy
-
 `Exp` -- `Cond` -- cond line, question, answer, else line
 
 # list processing
--- examples/tests/if.test.lisp
+++ examples/tests/if.test.lisp
@@ -1,8 +1,8 @@
 (assert (equal? (if #t 1 2) 1))
 (assert (equal? (if #f 1 2) 2))
 
-(assert (equal? (and #t #t #t) #t))
-(assert (equal? (and #t #t #f) #f))
+(assert (and #t #t #t))
+(assert (not (and #t #t #f)))
 
-(assert (equal? (or #f #f #t) #t))
-(assert (equal? (or #f #f #f) #f))
+(assert (or #f #f #t))
+(assert (not (or #f #f #f)))
