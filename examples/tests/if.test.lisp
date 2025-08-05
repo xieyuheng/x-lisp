86f8da78f541c3cc97a9ad592dc9371b59d52e09
Xie Yuheng <xyheme@gmail.com>
2025-08-06 06:14:15 +0800

    up

--- examples/tests/if.test.lisp
+++ examples/tests/if.test.lisp
@@ -1,2 +1,8 @@
 (assert (equal? (if #t 1 2) 1))
 (assert (equal? (if #f 1 2) 2))
+
+(assert (equal? (if (and #t #t #t) 1 2) 1))
+(assert (equal? (if (and #t #t #f) 1 2) 2))
+
+(assert (equal? (if (or #f #f #t) 1 2) 1))
+(assert (equal? (if (or #f #f #f) 1 2) 2))
