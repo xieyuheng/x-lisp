7e8a9acbfcfe419dbd70a6757c0123fafa153386
Xie Yuheng <xyheme@gmail.com>
2025-08-19 10:27:08 +0800

    up

---
+++ examples/module/import-rename.test.lisp
@@ -0,0 +1,5 @@
+(import "module-one.lisp" (rename one ione))
+(import "module-two.lisp" (rename two itwo))
+
+(assert (equal? ione 1))
+(assert (equal? itwo 2))
--- examples/module/import.test.lisp
+++ examples/module/import.test.lisp
@@ -3,9 +3,3 @@
 
 (assert (equal? one 1))
 (assert (equal? two 2))
-
-(import "module-one.lisp" (rename one ione))
-(import "module-two.lisp" (rename two itwo))
-
-(assert (equal? ione 1))
-(assert (equal? itwo 2))
