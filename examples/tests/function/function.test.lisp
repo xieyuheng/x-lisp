16ab2a71777e2190c139e899d0bc3d16fdbe730f
Xie Yuheng <xyheme@gmail.com>
2025-08-20 00:59:54 +0800

    up

--- examples/tests/function/function.test.lisp
+++ examples/tests/function/function.test.lisp
@@ -1,3 +1,5 @@
+;; pipe
+
 (assert (equal? (pipe/fn 0 []) 0))
 (assert (equal? (pipe/fn 0 [(iadd 1)]) 1))
 (assert (equal? (pipe/fn 0 [(iadd 1) (iadd 1)]) 2))
@@ -8,6 +10,14 @@
 (assert (equal? (pipe 0 (iadd 1) (iadd 1)) 2))
 (assert (equal? (pipe 0 (iadd 1) (iadd 1) (iadd 1)) 3))
 
+;; about the order of pipe
+
+(assert (equal?
+         (pipe 1 (iadd 1) (imul 2))
+         (imul 2 (iadd 1 1))))
+
+;; compose
+
 (assert (equal? (compose/fn [] 0) 0))
 (assert (equal? (compose/fn [(iadd 1)] 0) 1))
 (assert (equal? (compose/fn [(iadd 1) (iadd 1)] 0) 2))
@@ -17,3 +27,9 @@
 (assert (equal? ((compose (iadd 1)) 0) 1))
 (assert (equal? ((compose (iadd 1) (iadd 1)) 0) 2))
 (assert (equal? ((compose (iadd 1) (iadd 1) (iadd 1)) 0) 3))
+
+;; about the order of compose
+
+(assert (equal?
+         ((compose (iadd 1) (imul 2)) 1)
+         (iadd 1 (imul 2 1))))
