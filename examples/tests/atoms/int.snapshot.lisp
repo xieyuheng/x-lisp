a677d033132689a3b884f712223c5ed7da90ad50
Xie Yuheng <xyheme@gmail.com>
2025-08-25 05:09:42 +0800

    up

--- examples/tests/atoms/int.snapshot.lisp
+++ examples/tests/atoms/int.snapshot.lisp
@@ -2,17 +2,18 @@
 0
 -1
 
-(iadd 1 1)
-(iadd 1 -1)
-(iadd 0 0)
-(iadd 1 0)
-(iadd 0 1)
-
-(iadd 1 (iadd 2 3))
-(iadd (iadd 1 2) 3)
-
 (iadd 1)
-((iadd 1) 1)
+
+(assert-equal ((iadd 1) 1) 2)
+
+(assert-equal (iadd 1 1) 2)
+(assert-equal (iadd 1 -1) 0)
+(assert-equal (iadd 0 0) 0)
+(assert-equal (iadd 1 0) 1)
+(assert-equal (iadd 0 1) 1)
+
+(assert-equal (iadd 1 (iadd 2 3)) 6)
+(assert-equal (iadd (iadd 1 2) 3) 6)
 
 (assert-equal (ineg 1) -1)
 (assert-equal (ineg -1) 1)
