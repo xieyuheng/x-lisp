78ae9324bd0f0166518a5ffb5a6c17da451bdc96
Xie Yuheng <xyheme@gmail.com>
2025-08-25 07:07:39 +0800

    up

--- std/record/record-filter.lisp
+++ std/record/record-filter.lisp
@@ -5,7 +5,5 @@
 (define (record-filter record p)
   (pipe record
     record-entries
-    (swap list-filter (lambda (entry) (p (car (cdr entry)))))
-    ;; (swap list-filter (lambda (entry) (p (list-second entry))))
-    ;; (swap list-filter (compose p list-second))
+    (swap list-filter (compose p list-second))
     record-from-entries))
