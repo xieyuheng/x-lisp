000fdedc537f15fe9ec69738a98f22690d468b23
Xie Yuheng <xyheme@gmail.com>
2025-08-26 05:34:12 +0800

    up

--- src/lang/builtin/aboutProcess.ts
+++ src/lang/builtin/aboutProcess.ts
@@ -3,6 +3,7 @@ import {
   definePrimitiveFunction,
   definePrimitiveThunk,
 } from "../define/index.ts"
+import { formatValue } from "../format/index.ts"
 import { type Mod } from "../mod/index.ts"
 import * as Values from "../value/index.ts"
 
@@ -11,8 +12,9 @@ export function aboutProcess(mod: Mod) {
     return Values.String(process.cwd())
   })
 
-  definePrimitiveFunction(mod, "exit", 1, (message) => {
-    console.log(Values.asString(message).content)
+  definePrimitiveFunction(mod, "exit", 1, (info) => {
+    process.stdout.write(formatValue(info))
+    process.stdout.write("\n")
     process.exit(1)
   })
 }
