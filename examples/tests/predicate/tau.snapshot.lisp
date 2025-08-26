45ae04cf701fc2b2cf0c04fe85c5f02f2df2e3bb
Xie Yuheng <xyheme@gmail.com>
2025-08-26 14:06:07 +0800

    `validate` -- handle `Tau`

---
+++ examples/tests/predicate/tau.snapshot.lisp
@@ -0,0 +1,5 @@
+(tau int? int? :x int? :y int?)
+
+(assert ((tau int? int? :x int? :y int?) [1 2 :x 1 :y 2]))
+(assert-not ((tau int? int? :x int? :y int?) [1 2 :x 1 :y 'x]))
+(assert-not ((tau int? int?) [1 'x]))
--- src/lang/evaluate/validate.ts
+++ src/lang/evaluate/validate.ts
@@ -1,3 +1,4 @@
+import { arrayZip } from "../../utils/array/arrayZip.ts"
 import { formatValue } from "../format/index.ts"
 import * as Values from "../value/index.ts"
 import { type Value } from "../value/index.ts"
@@ -6,8 +7,49 @@ import { apply } from "./apply.ts"
 export type Result = { kind: "Ok"; value: Value } | { kind: "Err" }
 
 export function validate(schema: Value, value: Value): Result {
+  schema = Values.lazyWalk(schema)
+  value = Values.lazyWalk(value)
+
   if (schema.kind === "Arrow") {
     return { kind: "Ok", value: Values.The(schema, value) }
+  }
+
+  if (schema.kind === "Tau") {
+    if (value.kind !== "Tael") {
+      return { kind: "Err" }
+    }
+
+    if (schema.elementSchemas.length !== value.elements.length) {
+      return { kind: "Err" }
+    }
+
+    const validatedElements: Array<Value> = []
+    const zipped = arrayZip(schema.elementSchemas, value.elements)
+    for (const [elementSchema, element] of zipped) {
+      const result = validate(elementSchema, element)
+      if (result.kind === "Ok") {
+        validatedElements.push(result.value)
+      } else {
+        return { kind: "Err" }
+      }
+    }
+
+    const validatedAttributes: Record<string, Value> = {}
+    for (const key of Object.keys(schema.attributeSchemas)) {
+      const attributeSchema = schema.attributeSchemas[key]
+      const attribute = value.attributes[key] || Values.Null()
+      const result = validate(attributeSchema, attribute)
+      if (result.kind === "Ok") {
+        validatedAttributes[key] = result.value
+      } else {
+        return { kind: "Err" }
+      }
+    }
+
+    return {
+      kind: "Ok",
+      value: Values.Tael(validatedElements, validatedAttributes),
+    }
   }
 
   const result = apply(schema, [value])
