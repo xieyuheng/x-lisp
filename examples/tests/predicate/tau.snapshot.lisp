7966233e6d7e5d3b64fcb1ea17a89d376522bdee
Xie Yuheng <xyheme@gmail.com>
2025-08-26 14:07:29 +0800

    up

--- examples/tests/predicate/tau.snapshot.lisp
+++ examples/tests/predicate/tau.snapshot.lisp
@@ -3,3 +3,7 @@
 (assert ((tau int? int? :x int? :y int?) [1 2 :x 1 :y 2]))
 (assert-not ((tau int? int? :x int? :y int?) [1 2 :x 1 :y 'x]))
 (assert-not ((tau int? int?) [1 'x]))
+
+(assert-equal
+  (the (tau int? int?) [1 2])
+  [1 2])
