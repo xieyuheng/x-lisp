e2362d569d23ffce5f8570f68117e5488c65955d
Xie Yuheng <xyheme@gmail.com>
2025-09-05 01:36:31 +0800

    `random-float`

--- src/lang/builtin/aboutRandom.ts
+++ src/lang/builtin/aboutRandom.ts
@@ -1,14 +1,21 @@
+import { randomFloat } from "../../utils/random/randomFloat.ts"
 import { randomInt } from "../../utils/random/randomInt.ts"
 import { definePrimitiveFunction, provide } from "../define/index.ts"
 import { type Mod } from "../mod/index.ts"
 import * as Values from "../value/index.ts"
 
 export function aboutRandom(mod: Mod) {
-  provide(mod, ["random-int"])
+  provide(mod, ["random-int", "random-float"])
 
   definePrimitiveFunction(mod, "random-int", 2, (start, end) => {
     return Values.Int(
       randomInt(Values.asInt(start).content, Values.asInt(end).content),
+    )
+  })
+
+  definePrimitiveFunction(mod, "random-float", 2, (start, end) => {
+    return Values.Float(
+      randomFloat(Values.asFloat(start).content, Values.asFloat(end).content),
     )
   })
 }
