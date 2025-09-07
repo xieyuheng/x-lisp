18142f0bfa560a0d3494354c3538aef4b7d77796
Xie Yuheng <xyheme@gmail.com>
2025-09-07 15:37:17 +0800

    up

--- std/conditional/unless.lisp
+++ std/conditional/unless.lisp
@@ -1,3 +1,6 @@
 (export unless)
 
+;; (unless) can only take fixed number of arguments,
+;; just like (if).
+
 (define-lazy (unless p f) (if p void f))
--- std/conditional/when.lisp
+++ std/conditional/when.lisp
@@ -1,3 +1,6 @@
 (export when)
 
+;; (when) can only take fixed number of arguments,
+;; just like (if).
+
 (define-lazy (when p t) (if p t void))
