ceac51a388553e6210e20dc015f4f78132cc30a5
Xie Yuheng <xyheme@gmail.com>
2025-09-10 04:17:25 +0800

    run tests in parallel

--- scripts/run-std.sh
+++ scripts/run-std.sh
@@ -4,17 +4,6 @@ bin="node ./lib/main.js run --debug=true --no-std-prelude=true"
 ext=lisp
 dir=std
 
-for file in $(find $dir -name "*.test.${ext}"); do
-    echo "[test] $file"
-    ${bin} $file
-done
-
-for file in $(find $dir -name "*.snapshot.${ext}"); do
-    echo "[snapshot] $file"
-    ${bin} $file > $file.out
-done
-
-for file in $(find $dir -name "*.error.${ext}"); do
-    echo "[error] $file"
-    ${bin} $file > $file.err || true
-done
+find $dir -name "*.test.${ext}" | parallel -v ${bin} {}
+find $dir -name "*.snapshot.${ext}" | parallel -v ${bin} {} ">" {}.out
+find $dir -name "*.error.${ext}" | parallel -v ${bin} {} ">" {}.err "||" true
