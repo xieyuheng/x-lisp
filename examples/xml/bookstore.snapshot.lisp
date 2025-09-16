5cb35591fd340e7b9c5e7f2689ffc75171722b30
Xie Yuheng <xyheme@gmail.com>
2025-09-17 03:07:18 +0800

    up

--- examples/xml/bookstore.snapshot.lisp
+++ examples/xml/bookstore.snapshot.lisp
@@ -1,11 +1,11 @@
 '(bookstore
-  (book :id "b101" :category "fiction" :lang "en"
+  (book :id b101 :category fiction :lang en
    (title "The Great Novel")
    (author "Bill Authorette")
    (year 2021)
-   (price :currency "USD" 24.99))
-  (book :id "b102" :category "non-fiction" :lang "fr"
+   (price :currency USD 24.99))
+  (book :id b102 :category non-fiction :lang fr
    (title "Learning XML")
    (author "Pierre Document")
    (year 2019)
-   (price :currency "EUR" 19.50)))
+   (price :currency EUR 19.50)))
