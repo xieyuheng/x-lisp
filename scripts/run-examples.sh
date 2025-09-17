ffc3dcad38c313e793899936f0376d64d91eda00
Xie Yuheng <xyheme@gmail.com>
2025-09-18 01:16:18 +0800

    up

--- scripts/run-examples.sh
+++ scripts/run-examples.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
+set -e
+
 parallel="parallel -v --halt now,fail=1"
 bin="node ./lib/main.js run --debug=true"
 
-true &&
-    find examples -name "*.test.lisp" | $parallel $bin {} &&
-    find examples -name "*.snapshot.lisp" | $parallel $bin {} ">" {}.out &&
-    find examples -name "*.error.lisp" | $parallel $bin {} ">" {}.err "||" true
+find examples -name "*.test.lisp" | $parallel $bin {}
+find examples -name "*.snapshot.lisp" | $parallel $bin {} ">" {}.out
+find examples -name "*.error.lisp" | $parallel $bin {} ">" {}.err "||" true
--- scripts/run-std.sh
+++ scripts/run-std.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
+set -e
+
 parallel="parallel -v --halt now,fail=1"
 bin="node ./lib/main.js run --debug=true --no-std-prelude=true"
 
-true &&
-    find std -name "*.test.lisp" | $parallel $bin {} &&
-    find std -name "*.snapshot.lisp" | $parallel $bin {} ">" {}.out &&
-    find std -name "*.error.lisp" | $parallel $bin {} ">" {}.err "||" true
+find std -name "*.test.lisp" | $parallel $bin {}
+find std -name "*.snapshot.lisp" | $parallel $bin {} ">" {}.out
+find std -name "*.error.lisp" | $parallel $bin {} ">" {}.err "||" true
