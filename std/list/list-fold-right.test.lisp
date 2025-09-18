3e2b5e58eef896b6134493503d1e6b528f67c23a
Xie Yuheng <xyheme@gmail.com>
2025-09-18 19:41:08 +0800

    up

--- std/list/list-fold-right.test.lisp
+++ std/list/list-fold-right.test.lisp
@@ -1,4 +1,4 @@
 (import-all "list-fold-right.lisp")
 
 (assert-equal (list-fold-right iadd 0 [1 2 3 4]) 10)
-(assert-equal (list-fold-right  cons [] [1 2 3 4]) [1 2 3 4])
+(assert-equal (list-fold-right cons [] [1 2 3 4]) [1 2 3 4])
