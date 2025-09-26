8dc094969023be4b36ce26340953fadf26cea5e0
Xie Yuheng <xyheme@gmail.com>
2025-09-26 19:30:09 +0800

    `applyPolymorphic`

--- TODO.md
+++ TODO.md
@@ -1,12 +1,7 @@
-`applyPolymorphic` -- in `specific.ts`
 `validate` -- `Polymorphic` -- take `anything?` as arguments
-
 polymorphic.test.lisp
-
-`specific`
 
 add `Specific` to `Value`
 add `Specific` to `Exp`
 parse `(specific)`
-
-`apply` -- `Specific`
+`specific`
---
+++ src/lang/evaluate/applyPolymorphic.ts
@@ -0,0 +1,25 @@
+import { envSetValue } from "../env/index.ts"
+import { formatValue, formatValues } from "../format/index.ts"
+import type { Value } from "../value/index.ts"
+import * as Values from "../value/index.ts"
+import { evaluate, resultValue } from "./evaluate.ts"
+
+function applyPolymorphic(
+  polymorphic: Values.Polymorphic,
+  args: Array<Value>,
+): Value {
+  const arity = polymorphic.parameters.length
+  if (args.length !== arity) {
+    let message = `[applyPolymorphic] arity mismatch\n`
+    message += `  polymorphic: ${formatValue(polymorphic)}\n`
+    message += `  args: [${formatValues(args)}]\n`
+    throw new Error(message)
+  }
+
+  let env = polymorphic.env
+  for (const [index, parameter] of polymorphic.parameters.entries()) {
+    env = envSetValue(env, parameter, args[index])
+  }
+
+  return resultValue(evaluate(polymorphic.schema)(polymorphic.mod, env))
+}
