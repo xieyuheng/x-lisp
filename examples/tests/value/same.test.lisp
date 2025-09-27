1d2e9b46efad4d55c7d1e6e457f0500adfb6f5cf
Xie Yuheng <xyheme@gmail.com>
2025-09-28 02:04:05 +0800

    up

--- examples/tests/value/same.test.lisp
+++ examples/tests/value/same.test.lisp
@@ -4,6 +4,9 @@
 (assert (same? 'a 'a))
 (assert-not (same? 'a 'b))
 
+(assert (same? "abc" "abc"))
+(assert-not (same? "abc" "abcd"))
+
 (define abc '(a b c))
 (assert (same? abc abc))
 (assert-not (same? abc '(a b c)))
