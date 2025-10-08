1f8419926d2ff532f4b4b47ae2f83d38a60b8738
Xie Yuheng <xyheme@gmail.com>
2025-10-08 14:55:59 +0800

    `definePrimitiveVariadicFunction`

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
-`definePrimitiveVariadicFunction`
-
 `apply` -- handle `PrimitiveVariadicFunction`
 
 remove `*-concat` functions
---
+++ src/lang/define/definePrimitive.ts
@@ -0,0 +1,28 @@
+import { define } from "../define/index.ts"
+import { type Mod } from "../mod/index.ts"
+import * as Values from "../value/index.ts"
+
+export function definePrimitiveFunction(
+  mod: Mod,
+  name: string,
+  arity: number,
+  fn: Values.ValueFunction,
+): void {
+  define(mod, name, Values.PrimitiveFunction(name, arity, fn))
+}
+
+export function definePrimitiveVariadicFunction(
+  mod: Mod,
+  name: string,
+  fn: Values.ValueFunction,
+): void {
+  define(mod, name, Values.PrimitiveVariadicFunction(name, fn))
+}
+
+export function definePrimitiveThunk(
+  mod: Mod,
+  name: string,
+  fn: Values.ValueThunk,
+): void {
+  define(mod, name, Values.PrimitiveThunk(name, fn))
+}
