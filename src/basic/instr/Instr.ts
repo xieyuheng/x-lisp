b27d94381a19cf585a8ece3d9eb9b301847e5014
Xie Yuheng <xyheme@gmail.com>
2025-10-20 20:30:51 +0800

    [basic] `Instr` -- remove `Effect` and `Operation`

--- src/basic/instr/Instr.ts
+++ src/basic/instr/Instr.ts
@@ -7,19 +7,3 @@ export type Instr = {
   op: string
   operands: Array<Operand>
 }
-
-export type Operation = Instr & {
-  dest: string
-}
-
-export function isOperation(instr: Instr): instr is Operation {
-  return instr.dest !== undefined
-}
-
-export type Effect = Instr & {
-  dest: undefined
-}
-
-export function isEffect(instr: Instr): instr is Effect {
-  return instr.dest === undefined
-}
