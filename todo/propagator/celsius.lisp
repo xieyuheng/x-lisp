1908b39895a6042d5951959eeda5ea1f33992513
Xie Yuheng <xyheme@gmail.com>
2025-10-23 18:17:59 +0800

    up

--- todo/propagator/celsius.lisp
+++ todo/propagator/celsius.lisp
@@ -6,6 +6,15 @@
    (divider 5 9)
    c))
 
+;; maybe use -pp postfix to name all propagators,
+;; unified namespace is a problem here.
+
+(define-propagator (fahrenheit-to-celsius-pp f c)
+  (mul-pp
+   (sub-pp f 32)
+   (div-pp 5 9)
+   c))
+
 (define-propagator (fahrenheit-celsius f c)
   (= a (cell))
   (sum a 32 f)
