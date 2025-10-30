28eef80b56e005535dc00ced67a4073828486156
Xie Yuheng <xyheme@gmail.com>
2025-10-30 09:06:23 +0800

    move cicada-lisp to here

---
+++ design/cicada-lisp/structures/category/index.lisp
@@ -0,0 +1,5 @@
+(import category-t "category.lisp")
+(import functor-t "functor.lisp")
+(import naturaltrans-formation-t "naturaltrans-formation.lisp")
+(import isomorphism-t "isomorphism.lisp")
+(import trivial-category-t "trivial-category.lisp")
