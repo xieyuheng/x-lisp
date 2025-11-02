347dc6ab3fb0d48ddb1ef3ac6247d033c0113e68
Xie Yuheng <xyheme@gmail.com>
2025-11-03 05:00:48 +0800

    [backend] remove `Type`

--- compiler.js/src/compiler/backend/instr/Instr.ts
+++ compiler.js/src/compiler/backend/instr/Instr.ts
@@ -1,12 +1,10 @@
 import { type TokenMeta } from "@xieyuheng/x-sexp.js"
-import { type Type } from "../type/index.ts"
 import { type Operand } from "./Operand.ts"
 
 export type Meta = TokenMeta
 
 export type Instr = {
   dest?: string
-  type?: Type
   op: string
   operands: Array<Operand>
   meta?: Meta
