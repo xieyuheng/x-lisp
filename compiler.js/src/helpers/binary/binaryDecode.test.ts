8c9c8e6b66052c514031416caef7d1f631985133
Xie Yuheng <xyheme@gmail.com>
2025-11-07 02:25:44 +0800

    up

--- compiler.js/src/helpers/binary/binaryDecode.test.ts
+++ compiler.js/src/helpers/binary/binaryDecode.test.ts
@@ -11,3 +11,13 @@ test("binaryDecode", () => {
   ])
   assert.deepEqual(data, { x: 1, y: 2, z: 3 })
 })
+
+test("binaryDecode -- DependentSchema", () => {
+  const bytes = new Uint8Array([1, 2, 3])
+  const data = binaryDecode(bytes.buffer, [
+    () => ["x", "Uint8"],
+    () => ["y", "Uint8"],
+    () => ["z", "Uint8"],
+  ])
+  assert.deepEqual(data, { x: 1, y: 2, z: 3 })
+})
