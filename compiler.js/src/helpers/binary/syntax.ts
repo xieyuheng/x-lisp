49454542e68818849c9fafc7a08a4684c2f3b9da
Xie Yuheng <xyheme@gmail.com>
2025-11-07 03:57:00 +0800

    [binary] drop Exp postfix

--- compiler.js/src/helpers/binary/syntax.ts
+++ compiler.js/src/helpers/binary/syntax.ts
@@ -2,14 +2,14 @@ import type { Exp } from "./Exp.ts"
 import * as Exps from "./Exp.ts"
 import type { Type } from "./Type.ts"
 
-export function sequence(exps: Array<Exp>): Exps.SequenceExp {
-  return Exps.SequenceExp(exps)
+export function sequence(exps: Array<Exp>): Exps.Sequence {
+  return Exps.Sequence(exps)
 }
 
-export function attribute(name: string, type: Type): Exps.AttributeExp {
-  return Exps.AttributeExp(name, type)
+export function attribute(name: string, type: Type): Exps.Attribute {
+  return Exps.Attribute(name, type)
 }
 
-export function dependent(fn: (data: any) => Exp): Exps.DependentExp {
-  return Exps.DependentExp(fn)
+export function dependent(fn: (data: any) => Exp): Exps.Dependent {
+  return Exps.Dependent(fn)
 }
