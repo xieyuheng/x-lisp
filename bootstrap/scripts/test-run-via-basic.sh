a0ad2f2372896bedab8f0e8600acd03e52d5a8de
Xie Yuheng <xyheme@gmail.com>
2025-11-08 02:37:07 +0800

    up

--- bootstrap/scripts/test-run-via-basic.sh
+++ bootstrap/scripts/test-run-via-basic.sh
@@ -5,6 +5,6 @@ set -e
 parallel="parallel -v --halt now,fail=1"
 bin="node ./lib/main.js run-via-basic"
 
-find lisp/lang-tests -name "*.test.lisp" | $parallel $bin {}
-find lisp/lang-tests -name "*.snapshot.lisp" | $parallel $bin {} ">" {}.out
-find lisp/lang-tests -name "*.error.lisp" | $parallel $bin {} ">" {}.err "||" true
+find lisp/tests -name "*.test.lisp" | $parallel $bin {}
+find lisp/tests -name "*.snapshot.lisp" | $parallel $bin {} ">" {}.out
+find lisp/tests -name "*.error.lisp" | $parallel $bin {} ">" {}.err "||" true
