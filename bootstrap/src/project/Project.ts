9424b02d5f9f77588a8495ca93f9e4896f4898ca
Xie Yuheng <xyheme@gmail.com>
2025-11-09 13:33:20 +0800

    `projectSourceDirectory` & `projectOutputDirectory`

--- bootstrap/src/project/Project.ts
+++ bootstrap/src/project/Project.ts
@@ -1,7 +1,22 @@
+import Path from "path"
 import { type ProjectConfig } from "./ProjectConfig.ts"
 
 export type Project = {
   rootDirectory: string
   config: ProjectConfig
   sourceFiles: Array<string>
+}
+
+export function projectSourceDirectory(project: Project): string {
+  return Path.resolve(
+    project.rootDirectory,
+    project.config["build"]["source-directory"],
+  )
+}
+
+export function projectOutputDirectory(project: Project): string {
+  return Path.resolve(
+    project.rootDirectory,
+    project.config["build"]["output-directory"],
+  )
 }
