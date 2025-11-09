b9f75d5c6bb3baf9bb7781898fc5c2e6802c2488
Xie Yuheng <xyheme@gmail.com>
2025-11-09 09:54:23 +0800

    up

--- bootstrap/src/commands/RunViaBasicCommand.ts
+++ bootstrap/src/commands/RunViaBasicCommand.ts
@@ -5,37 +5,6 @@ import { errorReport } from "../helpers/error/errorReport.ts"
 import { createUrlOrFileUrl } from "../helpers/url/createUrlOrFileUrl.ts"
 import * as L from "../lang/index.ts"
 
-export const BasicRunCommand: Command = {
-  name: "basic:run",
-  description: "Run a basic-lisp file",
-  help(commander) {
-    let message = `The ${this.name} command run a basic-lisp file.`
-    message += `\n`
-    message += `\n  ${commander.name} ${this.name} <file>`
-    message += `\n`
-    return message
-  },
-
-  async run(commander) {
-    if (typeof commander.args[0] !== "string") {
-      let message = `[basic:run] expect the first argument to be a path`
-      message += `\n  first argument: ${commander.args[0]}`
-      throw new Error(message)
-    }
-
-    try {
-      const url = createUrlOrFileUrl(commander.args[0])
-      const dependencies = new Map()
-      const mod = B.load(url, dependencies)
-      const context = B.createContext(mod)
-      B.call(context, L.TopLevelComputationName, [])
-    } catch (error) {
-      console.log(errorReport(error))
-      process.exit(1)
-    }
-  },
-}
-
 export const RunViaBasicCommand: Command = {
   name: "run-via-basic",
   description: "Run x-lisp code via basic-lisp",
@@ -59,7 +28,7 @@ export const RunViaBasicCommand: Command = {
       const mod = L.load(url)
       const basicMod = compileToBasic(mod)
       const context = B.createContext(basicMod)
-      B.call(context, "main", [])
+      B.call(context, L.TopLevelComputationName, [])
     } catch (error) {
       console.log(error)
       console.log(errorReport(error))
