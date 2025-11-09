c2d98e9bb57b4f69360d51b2654d82b10cd5cf22
Xie Yuheng <xyheme@gmail.com>
2025-11-09 11:29:14 +0800

    [basic] inline `include`

--- bootstrap/src/basic/define/include.ts
+++
@@ -1,7 +0,0 @@
-import { type Definition } from "../definition/index.ts"
-import { type Mod } from "../mod/index.ts"
-
-export function include(mod: Mod, name: string, definition: Definition): void {
-  mod.definitions.set(name, definition)
-  mod.exported.add(name)
-}
