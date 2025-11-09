280b9058798b3e02925ee746398e291c955715fe
Xie Yuheng <xyheme@gmail.com>
2025-11-09 09:28:14 +0800

    rename bundle-basic to basic:bundle

--- bootstrap/src/main.ts
+++ bootstrap/src/main.ts
@@ -1,17 +1,17 @@
 #!/usr/bin/env -S node --stack-size=65536
 
 import { Commander } from "@xieyuheng/commander.js"
-import { BundleBasicCommand } from "./commands/BundleBasicCommand.ts"
+import { BasicBundleCommand } from "./commands/BasicBundleCommand.ts"
+import { BasicRunCommand } from "./commands/BasicRunCommand.ts"
 import { CompilePassesCommand } from "./commands/CompilePassesCommand.ts"
 import { CompileToBasicCommand } from "./commands/CompileToBasicCommand.ts"
-import { BasicRunCommand } from "./commands/BasicRunCommand.ts"
 import { RunViaBasicCommand } from "./commands/RunViaBasicCommand.ts"
 
 async function main() {
   const commander = new Commander()
 
   commander.use(BasicRunCommand)
-  commander.use(BundleBasicCommand)
+  commander.use(BasicBundleCommand)
   commander.use(RunViaBasicCommand)
   commander.use(CompilePassesCommand)
   commander.use(CompileToBasicCommand)
