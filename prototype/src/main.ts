4d669da33a908fb12de7f02675a0db0fcd252fdc
Xie Yuheng <xyheme@gmail.com>
2025-11-11 19:46:59 +0800

    [prototype] update command.js to use middleware

--- prototype/src/main.ts
+++ prototype/src/main.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S node --stack-size=65536
 
-import * as cmd from "@xieyuheng/command.js"
+import * as Cmd from "@xieyuheng/command.js"
 import { flags } from "./flags.ts"
 import { globals } from "./globals.ts"
 import { errorReport } from "./helpers/error/errorReport.ts"
@@ -11,44 +11,49 @@ import { startRepl } from "./services/startRepl.ts"
 
 const { version } = getPackageJson()
 
-const router = cmd.createRouter("x-lisp-proto", version)
+const router = Cmd.createRouter("x-lisp-proto", version)
 
-const routes = ["run file --debug --no-prelude", "repl --no-prelude"]
+router.defineRoutes(["run file --debug --no-prelude", "repl --no-prelude"])
 
-router.bind(routes, {
-  run: ([file], options, tokens) => {
-    globals.commandLineArgs = tokens
-
-    if (options["--debug"] !== undefined) flags["debug"] = true
-    if (options["--no-prelude"] !== undefined) flags["no-prelude"] = true
-
-    const url = createUrl(file)
-    load(url)
+router.defineHandlers({
+  run: {
+    middleware: [setupGlobals(), setupFlags()],
+    handler: ([file]) => load(createUrl(file)),
   },
-  repl: (args, options, tokens) => {
-    globals.commandLineArgs = tokens
-
-    flags["debug"] = true
-    if (options["--no-prelude"] !== undefined) flags["no-prelude"] = true
-
-    startRepl()
+  repl: {
+    middleware: [setupGlobals(), setupFlags(), enableDebug()],
+    handler() {
+      startRepl()
+    },
   },
 })
-
-// router.bind(routes, {
-//   run: {
-//     middleware: [setupGlobals(), setupFlags()],
-//     handler: ([file]) => load(createUrl(file)),
-//   },
-//   repl: {
-//     middleware: [setupGlobals(), setupFlags(), enableDebug()],
-//     handler: () => startRepl(),
-//   },
-// })
 
 try {
   await router.run(process.argv.slice(2))
 } catch (error) {
   console.log(errorReport(error))
   process.exit(1)
+}
+
+// Middleware
+
+function setupGlobals(): Cmd.Middleware {
+  return (args, options, context, continuation) => {
+    globals.commandLineArgs = context.tokens
+    return continuation(args, options, context)
+  }
+}
+
+function setupFlags(): Cmd.Middleware {
+  return (args, options, context, continuation) => {
+    if (options["--debug"] !== undefined) flags["debug"] = true
+    if (options["--no-prelude"] !== undefined) flags["no-prelude"] = true
+    return continuation(args, options, context)
+  }
+}
+function enableDebug(): Cmd.Middleware {
+  return (args, options, context, continuation) => {
+    flags["debug"] = true
+    return continuation(args, options, context)
+  }
 }
