44104dede5a5679994bb04cc4913d0003ffa31d2
Xie Yuheng <xyheme@gmail.com>
2025-11-12 01:01:07 +0800

    [machine] `transpile` -- `transpileToX86Assembly` -- fix indentation and section

--- bootstrap/lisp/machine-tests/hello.machine.s
+++ bootstrap/lisp/machine-tests/hello.machine.s
@@ -1,10 +1,12 @@
+        .text
+
 start:
 start.entry:
-movq $1, %rax
-movq $1, %rdi
-movq message, %rsi
-movq length, %rdx
-syscall 
-movq $60, %rax
-movq $0, %rdi
-syscall 
+        movq $1, %rax
+        movq $1, %rdi
+        movq message, %rsi
+        movq length, %rdx
+        syscall 
+        movq $60, %rax
+        movq $0, %rdi
+        syscall 
