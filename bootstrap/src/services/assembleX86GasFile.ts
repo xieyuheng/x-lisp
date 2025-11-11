eae6c4905093f915973d6df92e75a486939fce7f
Xie Yuheng <xyheme@gmail.com>
2025-11-12 02:42:44 +0800

    [machine] services/assembleX86GasFile

---
+++ bootstrap/src/services/assembleX86GasFile.ts
@@ -0,0 +1,26 @@
+import { systemShellRun } from "../helpers/system/systemShellRun.ts"
+
+export function assembleX86GasFile(file: string): void {
+  if (!file.endsWith(".s")) {
+    let message = `[assembleX86GasFile] expect file to end with .s`
+    message += `\n  file: ${file}`
+    throw new Error(message)
+  }
+
+  const objectFile = file.slice(-2) + ".o"
+  const executableFile = file.slice(-2)
+
+  {
+    const result = systemShellRun("as", [file, "-o", objectFile])
+    if (result.stdout) console.log(result.stdout)
+    if (result.stderr) console.error(result.stderr)
+    if (result.status !== 0) process.exit(result.status)
+  }
+
+  {
+    const result = systemShellRun("ld", [objectFile, "-o", executableFile])
+    if (result.stdout) console.log(result.stdout)
+    if (result.stderr) console.error(result.stderr)
+    if (result.status !== 0) process.exit(result.status)
+  }
+}
