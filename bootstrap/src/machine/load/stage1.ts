c5a37aaa446c0e0eba26cd400bff8651af77b09f
Xie Yuheng <xyheme@gmail.com>
2025-11-11 10:51:06 +0800

    up

--- bootstrap/src/machine/load/stage1.ts
+++ bootstrap/src/machine/load/stage1.ts
@@ -5,7 +5,6 @@ import { type Stmt } from "../stmt/index.ts"
 export function stage1(mod: Mod, stmt: Stmt): void {
   if (stmt.kind === "DefineCode") {
     const definition = Definitions.CodeDefinition(mod, stmt.name, stmt.blocks)
-
     mod.definitions.set(stmt.name, definition)
   }
 }
