544f409d675b3e9c9973aa340c7f119c3070185a
Xie Yuheng <xyheme@gmail.com>
2025-11-14 11:13:47 +0800

    up

--- bootstrap/src/project/loadProject.ts
+++ bootstrap/src/project/loadProject.ts
@@ -2,9 +2,9 @@ import Path from "node:path"
 import { loadProjectConfig } from "./loadProjectConfig.ts"
 import { Project } from "./Project.ts"
 
-export async function loadProject(configFile?: string): Promise<Project> {
+export function loadProject(configFile?: string): Project {
   configFile = configFile || Path.join(process.cwd(), "project.json")
-  const config = await loadProjectConfig(configFile)
+  const config = loadProjectConfig(configFile)
   const rootDirectory = Path.resolve(Path.dirname(configFile))
   return new Project(rootDirectory, config)
 }
