3c2b54e54b92fbdbb1b988be64d5ff51ce7f1eff
Xie Yuheng <xyheme@gmail.com>
2025-11-14 11:13:47 +0800

    up

--- bootstrap/src/project/loadProjectConfig.ts
+++ bootstrap/src/project/loadProjectConfig.ts
@@ -1,7 +1,7 @@
 import fs from "node:fs"
 import { type ProjectConfig, ProjectConfigSchema } from "./ProjectConfig.ts"
 
-export async function loadProjectConfig(file: string): Promise<ProjectConfig> {
+export function loadProjectConfig(file: string): ProjectConfig {
   const text = fs.readFileSync(file, "utf8")
   const data = JSON.parse(text)
   return ProjectConfigSchema.parse(data)
