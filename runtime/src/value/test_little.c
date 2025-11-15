da9d646b6e6c664f2cdee137115f6c2014dc0f33
Xie Yuheng <xyheme@gmail.com>
2025-11-15 10:23:53 +0800

    [runtime] `value` -- `x_void` & `x_null`

--- runtime/src/value/test_little.c
+++ runtime/src/value/test_little.c
@@ -12,5 +12,11 @@ test_little(void) {
     little_copy(x_false, string);
     assert(string_equal("#f", string));
 
+    little_copy(x_void, string);
+    assert(string_equal("#void", string));
+
+    little_copy(x_null, string);
+    assert(string_equal("#null", string));
+
     test_end();
 }
