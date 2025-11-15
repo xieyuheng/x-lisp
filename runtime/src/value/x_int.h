22ed3819e48b92516abaabd99abc55c02e910d0b
Xie Yuheng <xyheme@gmail.com>
2025-11-16 07:03:26 +0800

    [runtime] `x_int_positive_p` & `x_int_non_negative_p`

--- runtime/src/value/x_int.h
+++ runtime/src/value/x_int.h
@@ -17,4 +17,6 @@ value_t x_int_greater_p(value_t x, value_t y);
 value_t x_int_less_p(value_t x, value_t y);
 value_t x_int_greater_equal_p(value_t x, value_t y);
 value_t x_int_less_equal_p(value_t x, value_t y);
+value_t x_int_positive_p(value_t x);
+value_t x_int_non_negative_p(value_t x);
 value_t x_int_to_float(value_t x);
