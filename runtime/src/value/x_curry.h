c291d97f3966eef249a490349eb2f538b76cf175
Xie Yuheng <xyheme@gmail.com>
2025-11-16 10:14:51 +0800

    [runtime] `x_make_curry` & `x_curry_put_mut`

--- runtime/src/value/x_curry.h
+++ runtime/src/value/x_curry.h
@@ -13,4 +13,7 @@ struct curry_t {
 curry_t *make_curry(value_t target, size_t arity, size_t size);
 void curry_free(curry_t *self);
 
-void curry_put(curry_t *self, size_t index, value_t arg);
+void curry_put(curry_t *self, size_t index, value_t value);
+
+value_t x_make_curry(value_t target, value_t arity, value_t size);
+value_t x_curry_put_mut(value_t index, value_t value, value_t curry);
