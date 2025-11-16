c1a57c22b4af14ccbfd4eebd294e8d273722db6e
Xie Yuheng <xyheme@gmail.com>
2025-11-16 08:36:22 +0800

    [runtime] `x_print` & `x_println_non_void`

--- runtime/src/value/x_console.c
+++ runtime/src/value/x_console.c
@@ -5,3 +5,18 @@ x_newline(void) {
     printf("\n");
     return x_void;
 }
+
+value_t
+x_print(value_t x) {
+    value_print(x, stdout);
+    return x_void;
+}
+
+value_t
+x_println_non_void(value_t x) {
+    if (x != x_void) {
+        value_print(x, stdout);
+    }
+
+    return x_void;
+}
