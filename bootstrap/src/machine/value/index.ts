14e64b44f43bdbebf171c36245c7d1df0253206e
Xie Yuheng <xyheme@gmail.com>
2025-11-17 08:12:44 +0800

    [machine] `value` -- `tags`
    ```
        X_INT         = 0b000,
        X_FLOAT       = 0b001,
        X_LITTLE      = 0b010,
        X_ADDRESS     = 0b011,
        //            = 0b100,
        //            = 0b101,
        //            = 0b110,
        X_OBJECT      = 0b111,
    ```

--- bootstrap/src/machine/value/index.ts
+++ bootstrap/src/machine/value/index.ts
@@ -1,1 +1,3 @@
 export * from "./encode.ts"
+export * from "./tags.ts"
+
---
+++ bootstrap/src/machine/value/tags.ts
@@ -0,0 +1,8 @@
+export const IntTag         = 0b000
+export const FloatTag       = 0b001
+export const LittleTag      = 0b010
+export const AddressTag     = 0b011
+// export const ____        = 0b100
+// export const ____        = 0b101
+// export const ____        = 0b110
+export const ObjectTag      = 0b111
