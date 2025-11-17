deafc50ccc62b835e926321f8b26a03d30e65d11
Xie Yuheng <xyheme@gmail.com>
2025-11-17 16:08:10 +0800

    up

--- bootstrap/projects/x-lisp-test-suite/build/machine/int.snapshot.machine.x86.s
+++ bootstrap/projects/x-lisp-test-suite/build/machine/int.snapshot.machine.x86.s
@@ -56,7 +56,7 @@ _main.prolog:
         pushq %r13
         pushq %r14
         pushq %r15
-        subq $112, %rsp
+        subq $96, %rsp
         jmp _main.body
 _main.body:
         leaq x_println_non_void(%rip), %rdi
@@ -71,29 +71,19 @@ _main.body:
         movq $0, %rdx
         callq x_make_curry
         movq %rax, -72(%rbp)
-        leaq _square(%rip), %rdi
-        orq $3, %rdi
-        movq $8, %rsi
-        movq $0, %rdx
-        callq x_make_curry
-        movq %rax, -80(%rbp)
-        movq $24, -88(%rbp)
-        movq -80(%rbp), %rdi
+        movq $24, -80(%rbp)
+        movq -72(%rbp), %rdi
+        movq -80(%rbp), %rsi
+        callq x_apply_unary
+        movq %rax, -88(%rbp)
+        movq -64(%rbp), %rdi
         movq -88(%rbp), %rsi
         callq x_apply_unary
         movq %rax, -96(%rbp)
-        movq -72(%rbp), %rdi
-        movq -96(%rbp), %rsi
-        callq x_apply_unary
-        movq %rax, -104(%rbp)
-        movq -64(%rbp), %rdi
-        movq -104(%rbp), %rsi
-        callq x_apply_unary
-        movq %rax, -112(%rbp)
-        movq -112(%rbp), %rax
+        movq -96(%rbp), %rax
         jmp _main.epilog
 _main.epilog:
-        addq $112, %rsp
+        addq $96, %rsp
         popq %r15
         popq %r14
         popq %r13
--- bootstrap/projects/x-lisp-test-suite/src/int.snapshot.lisp
+++ bootstrap/projects/x-lisp-test-suite/src/int.snapshot.lisp
@@ -1,4 +1,4 @@
 (define (square x)
   (imul x x))
 
-(square (square 3))
+(square 3)
