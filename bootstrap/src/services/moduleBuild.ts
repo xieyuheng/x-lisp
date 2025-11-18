82c4c7a9b9d40dca43ee768a2a058a5f6489a765
Xie Yuheng <xyheme@gmail.com>
2025-11-18 19:31:16 +0800

    up

--- bootstrap/src/services/moduleBuild.ts
+++ bootstrap/src/services/moduleBuild.ts
@@ -1,25 +1,12 @@
-import Path from "node:path"
 import { createUrl } from "../helpers/url/createUrl.ts"
 import * as L from "../lang/index.ts"
-import { createProject, projectBuild } from "../project/index.ts"
+import { projectBuild, projectFromSourceFiles } from "../project/index.ts"
 
 export function moduleBuild(file: string) {
   const langMod = L.loadEntry(createUrl(file))
   const dependencyMods = Array.from(langMod.dependencies.values())
   const sourceFiles = dependencyMods.map((mod) => mod.url.pathname)
-
   const entryFile = langMod.url.pathname
-  const rootDirectory = Path.dirname(entryFile)
-  const project = createProject(rootDirectory, {
-    name: "*",
-    version: "*",
-    build: {
-      "source-directory": rootDirectory,
-    },
-  })
-  project.sourceIds = sourceFiles.map((file) =>
-    Path.relative(rootDirectory, file),
-  )
-
+  const project = projectFromSourceFiles(entryFile, sourceFiles)
   projectBuild(project)
 }
