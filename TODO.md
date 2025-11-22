bb8f0d35ad2ef07ee921b3dc97e6f724c6846a65
Xie Yuheng <xyheme@gmail.com>
2025-11-22 16:11:27 +0800

    [helpers.c] `where_printf` -- print line number

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-[helpers.c] where_printf -- print line number
 [helpers.c] support `.snapshot.c`
 [helpers.c] remove bin -- use `.exe.c` postfix
 [helpers.c] self test should not depends on commander
--- projects/helpers.c/src/test/test.h
+++ projects/helpers.c/src/test/test.h
@@ -3,13 +3,12 @@
 double test_time_millisecond(void);
 double test_time_passed_millisecond(double start_millisecond);
 
-#define test_start(...) ((void) 0)
-#define test_end(...) ((void) 0)
-
-#define test_start_log(...) \
+#define test_start(...)                                         \
     double test_start_millisecond = test_time_millisecond();    \
-    printf("[%s] start\n", __FILE__)
-#define test_end_log(...) printf("[%s] end: %.3f ms\n", __FILE__, test_time_passed_millisecond(test_start_millisecond))
+    printf("%s:%d -- start\n", __FILE__, __LINE__)
 
-#define where_printf(...) printf("%s: ", __FILE__); printf(__VA_ARGS__)
+#define test_end(...)                                                   \
+    printf("%s:%d -- end %.3f ms\n", __FILE__, __LINE__, test_time_passed_millisecond(test_start_millisecond))
+
+#define where_printf(...) printf("%s:%d ", __FILE__, __LINE__); printf(__VA_ARGS__)
 #define who_printf(...) printf("[%s] ", __func__); printf(__VA_ARGS__)
