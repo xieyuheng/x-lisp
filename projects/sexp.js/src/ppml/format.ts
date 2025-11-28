d1016c6ef54206c03e8d66e4f5d237625f0f61ec
Xie Yuheng <xyheme@gmail.com>
2025-11-28 15:12:39 +0800

    ppml -- format -- avoid recursion

--- projects/sexp.js/src/ppml/format.ts
+++ projects/sexp.js/src/ppml/format.ts
@@ -2,7 +2,9 @@ import * as pp from "./index.ts"
 
 export function format(maxWidth: number, node: pp.Node): string {
   const target: LayoutTarget = [0, "GroupingInline", pp.GroupNode(node)]
-  return layout(maxWidth, 0, [target])
+  const parts: Array<string> = []
+  layout(maxWidth, 0, [target], parts)
+  return parts.join("")
 }
 
 type GroupingMode = "GroupingInline" | "GroupingBlock"
@@ -13,89 +15,108 @@ function layout(
   maxWidth: number,
   cursor: number,
   targets: Array<LayoutTarget>,
-): string {
-  if (targets.length === 0) {
-    return ""
-  }
+  parts: Array<string>,
+): void {
+  while (targets.length > 0) {
+    const [[indentation, mode, node], ...restTargets] = targets
 
-  const [[indentation, mode, node], ...restTargets] = targets
+    switch (node.kind) {
+      case "NullNode": {
+        targets = restTargets
+        continue
+      }
 
-  switch (node.kind) {
-    case "NullNode": {
-      return layout(maxWidth, cursor, restTargets)
-    }
+      case "TextNode": {
+        parts.push(node.content)
+        cursor = cursor + node.content.length
+        targets = restTargets
+        continue
+      }
 
-    case "TextNode": {
-      return (
-        node.content +
-        layout(maxWidth, cursor + node.content.length, restTargets)
-      )
-    }
+      case "AppendNode": {
+        targets = [
+          [indentation, mode, node.leftChild],
+          [indentation, mode, node.rightChild],
+          ...restTargets,
+        ]
+        continue
+      }
 
-    case "AppendNode": {
-      return layout(maxWidth, cursor, [
-        [indentation, mode, node.leftChild],
-        [indentation, mode, node.rightChild],
-        ...restTargets,
-      ])
-    }
+      case "IndentNode": {
+        targets = [
+          [indentation + node.length, mode, node.child],
+          ...restTargets,
+        ]
+        continue
+      }
 
-    case "IndentNode": {
-      return layout(maxWidth, cursor, [
-        [indentation + node.length, mode, node.child],
-        ...restTargets,
-      ])
-    }
+      case "BreakNode": {
+        switch (mode) {
+          case "GroupingInline": {
+            parts.push(node.space)
+            ;((cursor = cursor + node.space.length), (targets = restTargets))
+            continue
+          }
 
-    case "BreakNode": {
-      switch (mode) {
-        case "GroupingInline": {
-          return (
-            node.space +
-            layout(maxWidth, cursor + node.space.length, restTargets)
-          )
-        }
-
-        case "GroupingBlock": {
-          return (
-            "\n" +
-            " ".repeat(indentation) +
-            layout(maxWidth, indentation, restTargets)
-          )
+          case "GroupingBlock": {
+            parts.push("\n" + " ".repeat(indentation))
+            cursor = indentation
+            targets = restTargets
+            continue
+          }
         }
       }
-    }
 
-    case "GroupNode": {
-      const groupingMode = fitInline(maxWidth - cursor, [node.child])
-        ? "GroupingInline"
-        : "GroupingBlock"
+      case "GroupNode": {
+        const groupingMode = fitInline(maxWidth - cursor, [node.child])
+          ? "GroupingInline"
+          : "GroupingBlock"
 
-      return layout(maxWidth, cursor, [
-        [indentation, groupingMode, node.child],
-        ...restTargets,
-      ])
+        targets = [[indentation, groupingMode, node.child], ...restTargets]
+        continue
+      }
     }
   }
 }
 
 function fitInline(width: number, nodes: Array<pp.Node>): boolean {
-  if (width < 0) return false
-  if (nodes.length === 0) return true
+  while (true) {
+    if (width < 0) return false
+    if (nodes.length === 0) return true
 
-  const [node, ...restNodes] = nodes
-  switch (node.kind) {
-    case "NullNode":
-      return fitInline(width, restNodes)
-    case "TextNode":
-      return fitInline(width - node.content.length, restNodes)
-    case "AppendNode":
-      return fitInline(width, [node.leftChild, node.rightChild, ...restNodes])
-    case "IndentNode":
-      return fitInline(width, [node.child, ...restNodes])
-    case "BreakNode":
-      return fitInline(width - node.space.length, restNodes)
-    case "GroupNode":
-      return fitInline(width, [node.child, ...restNodes])
+    const [node, ...restNodes] = nodes
+    switch (node.kind) {
+      case "NullNode": {
+        nodes = restNodes
+        continue
+      }
+
+      case "TextNode": {
+        width = width - node.content.length
+        nodes = restNodes
+        continue
+      }
+
+      case "AppendNode": {
+        nodes = [node.leftChild, node.rightChild, ...restNodes]
+        continue
+      }
+
+      case "IndentNode": {
+        nodes = [node.child, ...restNodes]
+        continue
+      }
+
+      case "BreakNode": {
+        width = width - node.space.length
+        nodes = restNodes
+        continue
+      }
+
+      case "GroupNode": {
+        nodes = [node.child, ...restNodes]
+        continue
+      }
+    }
   }
 }
