b173cf62805b6c367d689e90822fba57e13e6f81
Xie Yuheng <xyheme@gmail.com>
2025-12-01 19:05:34 +0800

    ensure function pointers are 8 aligned -- -falign-functions=8

--- projects/runtime.c/bin/runtime.c
+++ projects/runtime.c/bin/runtime.c
@@ -8,6 +8,10 @@ main(int argc, char *argv[]) {
     (void) argc;
     (void) argv;
 
+    // sanity checks
+    assert(sizeof(uint64_t) == sizeof(void *));
+    assert(sizeof(uint64_t) == sizeof(uintptr_t));
+
     file_disable_buffer(stdout);
     file_disable_buffer(stderr);
 
