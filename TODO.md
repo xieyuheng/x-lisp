9021852a1ba54ea7296ba86a57cddb04184d7576
Xie Yuheng <xyheme@gmail.com>
2025-12-04 22:49:19 +0800

    x-lisp-boot lang use S.createRouter

--- TODO.md
+++ TODO.md
@@ -1,4 +1,2 @@
-x-lisp-boot lang use S.createRouter
-
 setup x-forth.c
 setup new PROJECT-MANAGEMENT.md
--- projects/x-lisp-boot.js/src/lang/parse/parseStmt.ts
+++ projects/x-lisp-boot.js/src/lang/parse/parseStmt.ts
@@ -4,115 +4,92 @@ import * as Stmts from "../stmt/index.ts"
 import { type Stmt } from "../stmt/index.ts"
 import { parseExp } from "./parseExp.ts"
 
-export function parseStmt(sexp: S.Sexp): Stmt {
-  return S.match(
-    S.matcherChoice<Stmt>([
-      S.matcher(
-        "(cons* 'define (cons* name parameters) body)",
-        ({ name, parameters, body }, { sexp }) => {
-          const keyword = S.asTael(sexp).elements[1]
-          const meta = S.tokenMetaFromSexpMeta(keyword.meta)
-          return Stmts.DefineFunction(
-            S.symbolContent(name),
-            S.listElements(parameters).map(S.symbolContent),
-            Exps.BeginSugar(S.listElements(body).map(parseExp), meta),
-            meta,
-          )
-        },
-      ),
+export const parseStmt = S.createRouter<Stmt>({
+  "(cons* 'define (cons* name parameters) body)": (
+    { name, parameters, body },
+    { sexp },
+  ) => {
+    const keyword = S.asTael(sexp).elements[1]
+    const meta = S.tokenMetaFromSexpMeta(keyword.meta)
+    return Stmts.DefineFunction(
+      S.symbolContent(name),
+      S.listElements(parameters).map(S.symbolContent),
+      Exps.BeginSugar(S.listElements(body).map(parseExp), meta),
+      meta,
+    )
+  },
 
-      S.matcher("(cons* 'define name body)", ({ name, body }, { sexp }) => {
-        const keyword = S.asTael(sexp).elements[1]
-        const meta = S.tokenMetaFromSexpMeta(keyword.meta)
-        return Stmts.DefineConstant(
-          S.symbolContent(name),
-          Exps.BeginSugar(S.listElements(body).map(parseExp), meta),
-          meta,
-        )
-      }),
+  "(cons* 'define name body)": ({ name, body }, { sexp }) => {
+    const keyword = S.asTael(sexp).elements[1]
+    const meta = S.tokenMetaFromSexpMeta(keyword.meta)
+    return Stmts.DefineConstant(
+      S.symbolContent(name),
+      Exps.BeginSugar(S.listElements(body).map(parseExp), meta),
+      meta,
+    )
+  },
 
-      S.matcher("(cons* 'export names)", ({ names }, { meta }) => {
-        return Stmts.Export(S.listElements(names).map(S.symbolContent), meta)
-      }),
+  "(cons* 'export names)": ({ names }, { meta }) => {
+    return Stmts.Export(S.listElements(names).map(S.symbolContent), meta)
+  },
 
-      S.matcher("`(import-all ,source)", ({ source }, { meta }) => {
-        return Stmts.ImportAll(S.stringContent(source), meta)
-      }),
+  "`(import-all ,source)": ({ source }, { meta }) => {
+    return Stmts.ImportAll(S.stringContent(source), meta)
+  },
 
-      S.matcher("`(include-all ,source)", ({ source }, { meta }) => {
-        return Stmts.IncludeAll(S.stringContent(source), meta)
-      }),
+  "`(include-all ,source)": ({ source }, { meta }) => {
+    return Stmts.IncludeAll(S.stringContent(source), meta)
+  },
 
-      S.matcher(
-        "(cons* 'import source entries)",
-        ({ source, entries }, { meta }) => {
-          return Stmts.Import(
-            S.stringContent(source),
-            S.listElements(entries).map(S.symbolContent),
-            meta,
-          )
-        },
-      ),
+  "(cons* 'import source entries)": ({ source, entries }, { meta }) => {
+    return Stmts.Import(
+      S.stringContent(source),
+      S.listElements(entries).map(S.symbolContent),
+      meta,
+    )
+  },
 
-      S.matcher(
-        "(cons* 'include source names)",
-        ({ source, names }, { meta }) => {
-          return Stmts.Include(
-            S.stringContent(source),
-            S.listElements(names).map(S.symbolContent),
-            meta,
-          )
-        },
-      ),
+  "(cons* 'include source names)": ({ source, names }, { meta }) => {
+    return Stmts.Include(
+      S.stringContent(source),
+      S.listElements(names).map(S.symbolContent),
+      meta,
+    )
+  },
 
-      S.matcher(
-        "(cons* 'import-except source names)",
-        ({ source, names }, { meta }) => {
-          return Stmts.ImportExcept(
-            S.stringContent(source),
-            S.listElements(names).map(S.symbolContent),
-            meta,
-          )
-        },
-      ),
+  "(cons* 'import-except source names)": ({ source, names }, { meta }) => {
+    return Stmts.ImportExcept(
+      S.stringContent(source),
+      S.listElements(names).map(S.symbolContent),
+      meta,
+    )
+  },
 
-      S.matcher(
-        "(cons* 'include-except source names)",
-        ({ source, names }, { meta }) => {
-          return Stmts.IncludeExcept(
-            S.stringContent(source),
-            S.listElements(names).map(S.symbolContent),
-            meta,
-          )
-        },
-      ),
+  "(cons* 'include-except source names)": ({ source, names }, { meta }) => {
+    return Stmts.IncludeExcept(
+      S.stringContent(source),
+      S.listElements(names).map(S.symbolContent),
+      meta,
+    )
+  },
 
-      S.matcher(
-        "`(import-as ,source ,prefix)",
-        ({ source, prefix }, { meta }) => {
-          return Stmts.ImportAs(
-            S.stringContent(source),
-            S.symbolContent(prefix),
-            meta,
-          )
-        },
-      ),
+  "`(import-as ,source ,prefix)": ({ source, prefix }, { meta }) => {
+    return Stmts.ImportAs(
+      S.stringContent(source),
+      S.symbolContent(prefix),
+      meta,
+    )
+  },
 
-      S.matcher(
-        "`(include-as ,source ,prefix)",
-        ({ source, prefix }, { meta }) => {
-          return Stmts.IncludeAs(
-            S.stringContent(source),
-            S.symbolContent(prefix),
-            meta,
-          )
-        },
-      ),
+  "`(include-as ,source ,prefix)": ({ source, prefix }, { meta }) => {
+    return Stmts.IncludeAs(
+      S.stringContent(source),
+      S.symbolContent(prefix),
+      meta,
+    )
+  },
 
-      S.matcher("exp", ({ exp }, { meta }) => {
-        return Stmts.Compute(parseExp(exp), meta)
-      }),
-    ]),
-    sexp,
-  )
-}
+  exp: ({ exp }, { meta }) => {
+    return Stmts.Compute(parseExp(exp), meta)
+  },
+})
