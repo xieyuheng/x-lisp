c9cc494b631ed7d653e923f29879ec16ad35ae57
Xie Yuheng <xyheme@gmail.com>
2025-12-07 15:15:08 +0800

    [helpers.c] `string_builder_clear`

--- projects/helpers.c/src/string/string_builder.c
+++ projects/helpers.c/src/string/string_builder.c
@@ -21,3 +21,35 @@ string_builder_free(string_builder_t *self) {
     free(self->buffer);
     free(self);
 }
+
+static void
+string_builder_ensure_capacity(string_builder_t *self) {
+    if (self->length > self->max_length) {
+        self->buffer = reallocate(
+            self->buffer,
+            self->max_length,
+            self->max_length * 2);
+        self->max_length *= 2;
+        string_builder_ensure_capacity(self);
+    }
+}
+
+// void
+// string_builder_append_char(string_builder_t *self, char c) {
+
+// }
+
+// void
+// string_builder_append_string(string_builder_t *self, char *string) {
+
+// }
+
+void
+string_builder_clear(string_builder_t *self) {
+    self->length = 0;
+}
+
+// char *
+// string_builder_produce(string_builder_t *self) {
+
+// }
--- projects/helpers.c/src/string/string_builder.h
+++ projects/helpers.c/src/string/string_builder.h
@@ -2,3 +2,9 @@
 
 string_builder_t *make_string_builder(void);
 void string_builder_free(string_builder_t *self);
+
+void string_builder_append_char(string_builder_t *self, char c);
+void string_builder_append_string(string_builder_t *self, char *string);
+
+void string_builder_clear(string_builder_t *self);
+char *string_builder_produce(string_builder_t *self);
