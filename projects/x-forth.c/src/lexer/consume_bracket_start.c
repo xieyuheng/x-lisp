0e417f52e4598690db25b7d14ee4c092aa46ebe6
Xie Yuheng <xyheme@gmail.com>
2025-12-08 00:00:18 +0800

    Revert "[lexer] be able to config `quotation_mark_chars`"
    This reverts commit d281affcc9b5a4391d4ab3c9318ba9e7c456580f.

--- projects/x-forth.c/src/lexer/consume_bracket_start.c
+++ projects/x-forth.c/src/lexer/consume_bracket_start.c
@@ -2,7 +2,7 @@
 
 bool
 can_consume_bracket_start(lexer_t *lexer) {
-    return lexer_char_is_bracket_start(lexer_next_char(lexer));
+    return lexer_char_is_bracket_start(lexer, lexer_next_char(lexer));
 }
 
 char *
