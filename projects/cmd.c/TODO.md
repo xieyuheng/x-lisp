9aa3185eecfbd5334a28d47ec3eb05cb4ed3dd3c
Xie Yuheng <xyheme@gmail.com>
2025-12-08 21:23:06 +0800

    [cmd] `cmd_router_free`

--- projects/cmd.c/TODO.md
+++ projects/cmd.c/TODO.md
@@ -1,9 +1,10 @@
-[cmd] `cmd_make_router` -- setup `route_free`
-
 [cmd] `cmd_route_t`
 [cmd] `cmd_make_route`
 [cmd] `cmd_parse_route`
+
 [cmd] `cmd_route_free`
+[cmd] `cmd_make_router` -- setup `route_free`
+
 [cmd] `cmd_route_match`
 
 [cmd] `cmd_context_t`
--- projects/cmd.c/src/cmd/cmd_router.h
+++ projects/cmd.c/src/cmd/cmd_router.h
@@ -1,19 +1,12 @@
 #pragma once
 
-cmd_router_t *
-cmd_make_router(const char *name, const char *version) {
-    cmd_router_t *self = new(cmd_router_t);
-    self->name = name;
-    self->version = version;
-    self->routes = make_array_auto();
-    // self->routes = make_array_auto_with((free_fn_t *) route_free);
-    self->handlers = make_hash_with_string_keys();
-    return self;
-}
+struct cmd_router_t {
+    const char *name;
+    const char *version;
 
-void
-cmd_router_free(cmd_router_t *self) {
-    array_free(self->routes);
-    hash_free(self->handlers);
-    free(self);
-}
+    array_t *routes;
+    hash_t *handlers;
+};
+
+cmd_router_t *cmd_make_router(const char *name, const char *version);
+void cmd_router_free(cmd_router_t *self);
