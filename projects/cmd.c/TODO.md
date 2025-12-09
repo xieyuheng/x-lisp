02c1918ce73a1a313877ff733d0876b7bea332ff
Xie Yuheng <xyheme@gmail.com>
2025-12-09 17:32:52 +0800

    [cmd] `cmd_fn_t` -- take `ctx`

--- projects/cmd.c/TODO.md
+++ projects/cmd.c/TODO.md
@@ -1,4 +1,2 @@
-[cmd] `cmd_fn_t` -- take `ctx`
-
 [cmd] `cmd_parse_route`
 [cmd] `cmd_route_match` -- take `argc` `argv` and `ctx`
--- projects/cmd.c/src/cmd/types.h
+++ projects/cmd.c/src/cmd/types.h
@@ -3,3 +3,4 @@
 typedef struct cmd_router_t cmd_router_t;
 typedef struct cmd_route_t cmd_route_t;
 typedef struct cmd_ctx_t cmd_ctx_t;
+typedef void (cmd_fn_t)(cmd_ctx_t *ctx);
