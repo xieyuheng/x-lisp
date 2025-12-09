6bc82bcec9d7da5948d8a59f77e1f7da86201b58
Xie Yuheng <xyheme@gmail.com>
2025-12-09 18:17:14 +0800

    [cmd] `cmd_route_match` -- handle `options`

--- projects/cmd.c/TODO.md
+++ projects/cmd.c/TODO.md
@@ -1,3 +1,2 @@
-[cmd] `cmd_route_match` -- handle `options`
 [cmd] `cmd_parse_route`
 [cmd] setup `cmd-tester.exe.c`
--- projects/cmd.c/src/cmd/cmd_route.c
+++ projects/cmd.c/src/cmd/cmd_route.c
@@ -20,11 +20,25 @@ cmd_route_free(cmd_route_t *self) {
 
 void
 cmd_route_match(cmd_route_t *self, cmd_ctx_t *ctx) {
+    size_t base = 1;
     for (size_t i = 0; i < array_length(self->arg_names); i++) {
-        const char *arg = ctx->argv[1 + i];
+        const char *arg = ctx->argv[base + i];
         assert(arg);
         array_push(ctx->args, string_copy(arg));
     }
 
-    // TODO handle options
+    base += array_length(self->arg_names);
+    size_t index = base;
+    while (index < ctx->argc) {
+        const char *option_name = ctx->argv[base + index];
+        const char *option_value = ctx->argv[base + index + 1];
+        if (option_value == NULL || string_starts_with(option_value, "-")) {
+            record_insert(ctx->options, option_name, NULL);
+            index++;
+        } else {
+            record_insert(ctx->options, option_name, string_copy(option_value));
+            index++;
+            index++;
+        }
+    }
 }
