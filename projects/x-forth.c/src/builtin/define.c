38bff15f04312405bae4e255c0d3d65ba7727a3b
Xie Yuheng <xyheme@gmail.com>
2025-12-11 07:53:31 +0800

    [builtin] `x_define_variable`

--- projects/x-forth.c/src/builtin/define.c
+++ projects/x-forth.c/src/builtin/define.c
@@ -11,7 +11,11 @@ x_define_constant(vm_t *vm) {
 
 void
 x_define_variable(vm_t *vm) {
-    (void) vm;
+    value_t value = stack_pop(vm->value_stack);
+    token_t *token = list_shift(vm->tokens);
+    assert(token->kind == SYMBOL_TOKEN);
+    define_variable(vm->mod, token->content, value);
+    token_free(token);
 }
 
 void
