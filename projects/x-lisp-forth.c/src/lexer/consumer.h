b1f6db772abd2bf0b11f738ed274fc7676e22e9c
Xie Yuheng <xyheme@gmail.com>
2025-12-18 17:59:56 +0800

    up

--- projects/x-lisp-forth.c/src/lexer/consumer.h
+++ projects/x-lisp-forth.c/src/lexer/consumer.h
@@ -5,9 +5,7 @@ typedef char *(consume_fn_t)(lexer_t *lexer);
 
 struct consumer_t {
     bool is_ignored;
-    union {
-        token_kind_t kind;
-    };
+    token_kind_t kind;
     can_consume_fn_t *can_consume;
     consume_fn_t *consume;
 };
