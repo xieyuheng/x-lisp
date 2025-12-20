01d5813d70d75c1517debad76b42f43f5f8171e4
Xie Yuheng <xyheme@gmail.com>
2025-12-21 05:15:28 +0800

    [gc] `gc_add_root`

--- projects/x-lisp-forth.c/src/gc/gc.c
+++ projects/x-lisp-forth.c/src/gc/gc.c
@@ -1,16 +1,29 @@
 #include "index.h"
 
+struct gc_t {
+    array_t *allocated_objects;
+    stack_t *work_stack;
+};
+
 gc_t *
 make_gc(void) {
     gc_t *self = new(gc_t);
     self->allocated_objects = make_array_auto_with(free);
-    self->gray_object_stack = make_stack();
+    self->work_stack = make_stack();
     return self;
 }
 
 void
 gc_free(gc_t *self) {
     array_free(self->allocated_objects);
-    stack_free(self->gray_object_stack);
+    stack_free(self->work_stack);
     free(self);
 }
+
+void
+gc_add_root(gc_t *self, object_t *root) {
+    stack_push(self->work_stack, root);
+}
+
+// gc_mark_object
+// gc_unmark_object
--- projects/x-lisp-forth.c/src/gc/gc.h
+++ projects/x-lisp-forth.c/src/gc/gc.h
@@ -1,9 +1,9 @@
 #pragma once
 
-struct gc_t {
-    array_t *allocated_objects;
-    stack_t *gray_object_stack;
-};
-
 gc_t *make_gc(void);
 void gc_free(gc_t *self);
+
+void gc_add_root(gc_t *self, object_t *root);
+
+void gc_mark(gc_t *self);
+void gc_sweep(gc_t *self);
