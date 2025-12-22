4c448a5e21b8bd614fcff66a7666e7793fc3a840
Xie Yuheng <xyheme@gmail.com>
2025-12-22 20:32:05 +0800

    [helpers.c] `record` -- remove iter state and API functions

--- projects/helpers.c/TODO.md
+++ projects/helpers.c/TODO.md
@@ -1,10 +1,3 @@
-# record
-
-[helpers.c] `record_iter_next_entry`
-[helpers.c] `record_iter_next`
-
-[helpers.c] `record` -- remove iter state and API functions
-
 # set
 
 [helpers.c] `set_iter_t`
--- projects/helpers.c/src/record/record_iter.test.c
+++ projects/helpers.c/src/record/record_iter.test.c
@@ -4,78 +4,81 @@ int
 main(void) {
     test_start();
 
-    // {
-    //     record_t *record = make_record();
-    //     assert(!record_first_value(record));
+    {
+        record_t *record = make_record();
 
-    //     //  Insert some entries
+        //  Insert some entries
 
-    //     array_t *keys = make_string_array();
-    //     array_push(keys, string_copy("DEADBEEF"));
-    //     array_push(keys, string_copy("ABADCAFE"));
-    //     array_push(keys, string_copy("C0DEDBAD"));
-    //     array_push(keys, string_copy("DEADF00D"));
+        array_t *keys = make_string_array();
+        array_push(keys, string_copy("DEADBEEF"));
+        array_push(keys, string_copy("ABADCAFE"));
+        array_push(keys, string_copy("C0DEDBAD"));
+        array_push(keys, string_copy("DEADF00D"));
 
-    //     array_t *values = make_string_array();
-    //     array_push(values, string_copy("dead beef"));
-    //     array_push(values, string_copy("a bad cafe"));
-    //     array_push(values, string_copy("coded bad"));
-    //     array_push(values, string_copy("dead food"));
+        array_t *values = make_string_array();
+        array_push(values, string_copy("dead beef"));
+        array_push(values, string_copy("a bad cafe"));
+        array_push(values, string_copy("coded bad"));
+        array_push(values, string_copy("dead food"));
 
-    //     assert(record_insert(record, array_get(keys, 0), array_get(values, 0)));
-    //     assert(record_insert(record, array_get(keys, 1), array_get(values, 1)));
-    //     assert(record_insert(record, array_get(keys, 2), array_get(values, 2)));
-    //     assert(record_insert(record, array_get(keys, 3), array_get(values, 3)));
+        assert(record_insert(record, array_get(keys, 0), array_get(values, 0)));
+        assert(record_insert(record, array_get(keys, 1), array_get(values, 1)));
+        assert(record_insert(record, array_get(keys, 2), array_get(values, 2)));
+        assert(record_insert(record, array_get(keys, 3), array_get(values, 3)));
 
-    //     assert(record_length(record) == 4);
+        assert(record_length(record) == 4);
 
-    //     {
-    //         array_t *keys_again = record_keys(record);
-    //         assert(string_equal(array_get(keys, 0), array_get(keys_again, 0)));
-    //         assert(string_equal(array_get(keys, 1), array_get(keys_again, 1)));
-    //         assert(string_equal(array_get(keys, 2), array_get(keys_again, 2)));
-    //         assert(string_equal(array_get(keys, 3), array_get(keys_again, 3)));
-    //         array_free(keys_again);
-    //     }
+        {
+            array_t *keys_again = record_keys(record);
+            assert(string_equal(array_get(keys, 0), array_get(keys_again, 0)));
+            assert(string_equal(array_get(keys, 1), array_get(keys_again, 1)));
+            assert(string_equal(array_get(keys, 2), array_get(keys_again, 2)));
+            assert(string_equal(array_get(keys, 3), array_get(keys_again, 3)));
+            array_free(keys_again);
+        }
 
-    //     {
-    //         array_t *values_again = record_values(record);
-    //         assert(string_equal(array_get(values, 0), array_get(values_again, 0)));
-    //         assert(string_equal(array_get(values, 1), array_get(values_again, 1)));
-    //         assert(string_equal(array_get(values, 2), array_get(values_again, 2)));
-    //         assert(string_equal(array_get(values, 3), array_get(values_again, 3)));
-    //         array_free(values_again);
-    //     }
+        {
+            array_t *values_again = record_values(record);
+            assert(string_equal(array_get(values, 0), array_get(values_again, 0)));
+            assert(string_equal(array_get(values, 1), array_get(values_again, 1)));
+            assert(string_equal(array_get(values, 2), array_get(values_again, 2)));
+            assert(string_equal(array_get(values, 3), array_get(values_again, 3)));
+            array_free(values_again);
+        }
 
-    //     // iterate by insertion order.
+        // iterate by insertion order.
 
-    //     {
-    //         size_t i = 0;
-    //         const char *key = record_first_key(record);
-    //         while (key) {
-    //             assert(string_equal(key, array_get(keys, i)));
-    //             key = record_next_key(record);
-    //             i++;
-    //         }
-    //     }
+        {
+            size_t i = 0;
+            record_iter_t iter;
+            record_iter_init(&iter, record);
+            const char *key = record_iter_next_key(&iter);
+            while (key) {
+                assert(string_equal(key, array_get(keys, i)));
+                key = record_iter_next_key(&iter);
+                i++;
+            }
+        }
 
-    //     {
-    //         size_t i = 0;
-    //         char *value = record_first_value(record);
-    //         while (value) {
-    //             assert(string_equal(value, array_get(values, i)));
-    //             value = record_next_value(record);
-    //             i++;
-    //         }
-    //     }
+        {
+            size_t i = 0;
+            record_iter_t iter;
+            record_iter_init(&iter, record);
+            char *value = record_iter_next_value(&iter);
+            while (value) {
+                assert(string_equal(value, array_get(values, i)));
+                value = record_iter_next_value(&iter);
+                i++;
+            }
+        }
 
-    //     array_free(keys);
-    //     array_free(values);
+        array_free(keys);
+        array_free(values);
 
-    //     record_purge(record);
-    //     assert(record_length(record) == 0);
-    //     record_free(record);
-    // }
+        record_purge(record);
+        assert(record_length(record) == 0);
+        record_free(record);
+    }
 
     test_end();
 }
