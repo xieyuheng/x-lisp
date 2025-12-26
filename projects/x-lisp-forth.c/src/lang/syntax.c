42d59d8a5412b02207163c58ad3a167b6acb08c5
Xie Yuheng <xyheme@gmail.com>
2025-12-26 10:21:35 +0800

    up

--- projects/x-lisp-forth.c/src/lang/syntax.c
+++ projects/x-lisp-forth.c/src/lang/syntax.c
@@ -1,9 +1,26 @@
 #include "index.h"
 
-struct syntax_entry_t { const char *name; x_fn_t *handler; };
+static void
+syntax_var(vm_t *vm) {
+    token_t *token = vm_next_token(vm);
+    assert(token->kind == SYMBOL_TOKEN);
+    function_t *function = make_function();
+    define_variable_setup(vm_mod(vm), token->content, function);
+    compile_function(vm, function);
+    token_free(token);
+}
 
-static void syntax_var(vm_t *vm);
-static void syntax_def(vm_t *vm);
+static void
+syntax_def(vm_t *vm) {
+    token_t *token = vm_next_token(vm);
+    assert(token->kind == SYMBOL_TOKEN);
+    function_t *function = make_function();
+    define_function(vm_mod(vm), token->content, function);
+    compile_function(vm, function);
+    token_free(token);
+}
+
+struct syntax_entry_t { const char *name; x_fn_t *handler; };
 
 static struct syntax_entry_t syntax_entries[] = {
     { "@var", syntax_var },
@@ -25,24 +42,4 @@ syntax_find_handler(const char *name) {
     }
 
     return NULL;
-}
-
-void
-syntax_var(vm_t *vm) {
-    token_t *token = vm_next_token(vm);
-    assert(token->kind == SYMBOL_TOKEN);
-    function_t *function = make_function();
-    define_variable_setup(vm_mod(vm), token->content, function);
-    compile_function(vm, function);
-    token_free(token);
-}
-
-void
-syntax_def(vm_t *vm) {
-    token_t *token = vm_next_token(vm);
-    assert(token->kind == SYMBOL_TOKEN);
-    function_t *function = make_function();
-    define_function(vm_mod(vm), token->content, function);
-    compile_function(vm, function);
-    token_free(token);
 }
