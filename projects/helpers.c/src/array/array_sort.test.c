34e8e98f49ff1ee152efc995cd214d8a9ab0f7b5
Xie Yuheng <xyheme@gmail.com>
2025-12-30 09:03:39 +0800

    up

--- projects/helpers.c/src/array/array_sort.test.c
+++ projects/helpers.c/src/array/array_sort.test.c
@@ -43,5 +43,28 @@ main(void) {
         array_free(array);
     }
 
+    {
+        array_t *array = make_array();
+        array_put(array, 0, (void *) "A");
+        array_put(array, 1, (void *) "BC");
+        array_put(array, 2, (void *) "B");
+        array_put(array, 3, (void *) "C");
+        array_put(array, 4, (void *) "ABC");
+        array_put(array, 5, (void *) "AB");
+        array_put(array, 6, (void *) "CB");
+
+        array_sort(array, string_compare_lexical);
+
+        assert(array_get(array, 0) == (void *) "A");
+        assert(array_get(array, 1) == (void *) "AB");
+        assert(array_get(array, 2) == (void *) "ABC");
+        assert(array_get(array, 3) == (void *) "B");
+        assert(array_get(array, 4) == (void *) "BC");
+        assert(array_get(array, 5) == (void *) "C");
+        assert(array_get(array, 6) == (void *) "CB");
+
+        array_free(array);
+    }
+
     test_end();
 }
