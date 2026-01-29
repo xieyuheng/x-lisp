380a29f32ed6c677ea000cee0a7066ba8890db6e
Xie Yuheng <xyheme@gmail.com>
2026-01-29 13:25:56 +0800

    up

---
+++ projects/x-lisp-compiler.js/lisp/test-suite/src/int/factorial.test.lisp
@@ -0,0 +1,12 @@
+(define (factorial n)
+  (if (int-less-or-equal? n 1)
+    1
+    (imul (factorial (isub n 1)) n)))
+
+(define (main)
+  (assert-equal 1 (factorial 0))
+  (assert-equal 1 (factorial 1))
+  (assert-equal 2 (factorial 2))
+  (assert-equal 6 (factorial 3))
+  (assert-equal 24 (factorial 4))
+  (assert-equal 120 (factorial 6)))
