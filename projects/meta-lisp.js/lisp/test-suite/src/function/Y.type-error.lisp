ae579624087ab33216ceb7f893cac7d9cb8ee1c8
Xie Yuheng <xyheme@gmail.com>
2026-03-07 18:17:08 +0800

    up

--- projects/meta-lisp.js/lisp/test-suite/src/function/Y.type-error.lisp
+++ projects/meta-lisp.js/lisp/test-suite/src/function/Y.type-error.lisp
@@ -5,9 +5,3 @@
 (define (Y f)
   ((lambda (u) (u u))
    (lambda (x) (f (lambda (t) ((x x) t))))))
-
-;; (define (Y f)
-;;   ((the (polymorphic (B) (-> (mu (X) (-> X B)) B))
-;;      (lambda (u) (u u)))
-;;    (the (polymorphic (A) (-> (mu (Y) (-> Y A)) A))
-;;      (lambda (x) (f (lambda (t) ((x x) t)))))))
