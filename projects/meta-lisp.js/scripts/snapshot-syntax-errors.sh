5fb652ae3c4d1caa2c9d1181fbc04d41fb90e58d
Xie Yuheng <xyheme@gmail.com>
2026-03-30 19:35:54 +0800

    up

--- projects/meta-lisp.js/scripts/snapshot-syntax-errors.sh
+++ projects/meta-lisp.js/scripts/snapshot-syntax-errors.sh
@@ -3,6 +3,6 @@
 set -e
 
 parallel="parallel -v --halt now,fail=1"
-bin="node ./lib/main.js module:check"
+bin="node ./src/main.ts module:check"
 
 find lib/syntax-errors -name "*.syntax-error.meta" | $parallel $bin {} ">" {}.out "||" true
