b2e8b22e307230fb21ac7e7c78b0be6923d27d31
Xie Yuheng <xyheme@gmail.com>
2026-03-30 18:56:05 +0800

    rename meta-lisp to meta-lisp.meta

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,4 +10,4 @@ pnpm run -r test
 pnpm run -r test:cli
 
 cd projects/meta-test-suite; bash scripts/test.sh; cd ../..
-cd projects/meta-lisp; bash scripts/test.sh; cd ../..
+cd projects/meta-lisp.meta; bash scripts/test.sh; cd ../..
