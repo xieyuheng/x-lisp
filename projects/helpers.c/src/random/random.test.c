138fcfb4c20bedf78e20649a9cba8fb50ff3cd10
Xie Yuheng <xyheme@gmail.com>
2026-05-04 10:37:48 +0800

    up

--- projects/helpers.c/src/random/random.test.c
+++ projects/helpers.c/src/random/random.test.c
@@ -3,11 +3,11 @@
 int main(void) {
   test_start();
 
-  for (size_t i = 0; i < 100; i++) {
+  for (size_t i = 0; i < 10; i++) {
     who_printf("random_int(0, 10): %ld\n", random_int(0, 10));
   }
 
-  for (size_t i = 0; i < 100; i++) {
+  for (size_t i = 0; i < 10; i++) {
     who_printf("random_float(0, 10): %f\n", random_float(0, 10));
   }
 
