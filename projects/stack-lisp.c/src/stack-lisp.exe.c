57b794dd869823f7477eb008ad41d5d7d878aff1
Xie Yuheng <xyheme@gmail.com>
2026-05-05 08:41:21 +0800

    [stack-lisp.c] remove run command

--- projects/stack-lisp.c/src/stack-lisp.exe.c
+++ projects/stack-lisp.c/src/stack-lisp.exe.c
@@ -5,13 +5,6 @@ static void sanity_check(void) {
   assert(sizeof(uint64_t) == sizeof(size_t));
 }
 
-static void handle_run(cmd_ctx_t *ctx) {
-  char *pathname = cmd_get_arg(ctx, 0);
-  bool profile = cmd_has_option(ctx, "--profile");
-  mod_t *mod = stk_load(make_path(pathname), profile);
-  (void) mod;
-}
-
 static void handle_call(cmd_ctx_t *ctx){
   char *pathname = cmd_get_arg(ctx, 0);
   char *name = cmd_get_arg(ctx, 1);
@@ -41,11 +34,9 @@ int main(int argc, char *argv[]) {
 
   cmd_router_t *router = cmd_make_router("stack-lisp", "0.1.0");
 
-  cmd_define_route(router, "run file --profile");
   cmd_define_route(router, "call file function --profile");
   cmd_define_route(router, "test file --profile --snapshot --builtin");
 
-  cmd_define_handler(router, "run", handle_run);
   cmd_define_handler(router, "call", handle_call);
   cmd_define_handler(router, "test", handle_test);
 
