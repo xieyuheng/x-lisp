2051a3aff4917590d33e7960478062beee518b69
Xie Yuheng <xyheme@gmail.com>
2026-05-15 14:44:06 +0800

    [meta-lisp.js] desugarLetrecStar 在代码前，用注释给出翻译例子，例子使用 box 而不是 list
    ```scheme
    (letrec* ((x1 e1)
              (x2 e2)
              ...
              (xn en))
      body)
    ```
    
    编译成：
    
    ```scheme
    (let ((x1 (make-box))
          (x2 (make-box))
          ...
          (xn (make-box)))
      (box-put! e1 x1)
      (box-put! e2 x2)
      ...
      (box-put! en xn)
      body)
    ```
    
    其中 e1 e2 en 和 body
    中的 x1 x2 xn
    都要替换为 (box-get x1) (box-get x2) (box-get xn)
    
    注意：
    
    - 你可以润色一下例子中的措辞，让例子更清晰。

--- TODO.md
+++ TODO.md
@@ -1,35 +1,3 @@
-[meta-lisp.js] desugarLetrecStar 在代码前，用注释给出翻译例子，例子使用 box 而不是 list
-
-```scheme
-(letrec* ((x1 e1)
-          (x2 e2)
-          ...
-          (xn en))
-  body)
-```
-
-编译成：
-
-```scheme
-(let ((x1 (make-box))
-      (x2 (make-box))
-      ...
-      (xn (make-box)))
-  (box-put! e1 x1)
-  (box-put! e2 x2)
-  ...
-  (box-put! en xn)
-  body)
-```
-
-其中 e1 e2 en 和 body
-中的 x1 x2 xn
-都要替换为 (box-get x1) (box-get x2) (box-get xn)
-
-注意：
-
-- 你可以润色一下例子中的措辞，让例子更清晰。
-
 [meta-lisp.js] 修复 desugarLetrecStar，使用 box 而不是 list，按照注释所描述的方式翻译。
 
 [meta-lisp.js] 修复 letrec 的 desugarLetrec，在代码中给出翻译例子
--- projects/meta-lisp.js/src/meta/passes/020-DesugarPass.ts
+++ projects/meta-lisp.js/src/meta/passes/020-DesugarPass.ts
@@ -286,6 +286,30 @@ function desugarLetrec(
   return M.LetrecStar([...thunkBindings, ...callBindings], body, location)
 }
 
+// Desugar `(letrec*)` using box:
+//
+//   (letrec* ((x1 e1)
+//             (x2 e2)
+//             ...
+//             (xn en))
+//     body)
+//
+// → where e1, e2, en, and body have their
+//   x1, x2, xn replaced with (box-get x1), (box-get x2), (box-get xn)
+//
+//   (let ((x1 (make-box))
+//         (x2 (make-box))
+//         ...
+//         (xn (make-box)))
+//     (box-put! e1 x1)
+//     (box-put! e2 x2)
+//     ...
+//     (box-put! en xn)
+//     body)
+//
+// The actual implementation uses list primitives
+// (make-list, car, list-put!) instead of box.
+
 function desugarLetrecStar(
   bindings: Array<M.Binding>,
   body: M.Exp,
