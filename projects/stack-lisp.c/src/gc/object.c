1b1a144ed358b8bd2389436eac70c0715351ff83
Xie Yuheng <xyheme@gmail.com>
2026-05-18 14:43:19 +0800

    up

--- projects/stack-lisp.c/src/gc/object.c
+++ projects/stack-lisp.c/src/gc/object.c
@@ -8,13 +8,26 @@ void object_free(object_t *self) {
 }
 
 void object_format(buffer_t *buffer, object_circle_ctx_t *ctx, object_t *self) {
+  if (self == NULL) {
+    format_template(buffer, "#(<null-object>)");
+    return;
+  }
+
+  if (self->header.class == NULL) {
+    format_template(buffer, "#(<classless> 0x%p)", (void *) self);
+    return;
+  }
+
   if (self->header.class->format_fn) {
     self->header.class->format_fn(buffer, ctx, self);
     return;
   }
 
-  format_template(buffer, "#(%s 0x%p)", self->header.class->name, (void *) self);
-  return;
+  if (self->header.class->name) {
+    format_template(buffer, "#(%s 0x%p)", self->header.class->name, (void *) self);
+  } else {
+    format_template(buffer, "#(<unnamed> 0x%p)", (void *) self);
+  }
 }
 
 void object_print(object_circle_ctx_t *ctx, object_t *self) {
