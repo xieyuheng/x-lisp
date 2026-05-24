d410f2943433981609d141ff0a4c3f48a1d9a5e3
Xie Yuheng <xyheme@gmail.com>
2026-05-24 23:55:02 +0800

    up

--- TODO.md
+++ TODO.md
@@ -1,3 +1,5 @@
+[meta-lisp.js] project.json 中所描述的 entry 没有被编译为 xasm 的 (default-entry)
+
 [meta-lisp.meta] term vs exp
 
 [meta-lisp.meta] term-free-names.meta
--- projects/xvm.c/scripts/run-tests.sh
+++ projects/xvm.c/scripts/run-tests.sh
@@ -5,5 +5,4 @@ set -e
 parallel="parallel -v --halt now,fail=1"
 
 find lib/tests -name "*.xasm" | $parallel ./src/xvm.exe assemble {}
-
 find lib/tests -name "*.xexe" | $parallel ./src/xvm.exe test {}
