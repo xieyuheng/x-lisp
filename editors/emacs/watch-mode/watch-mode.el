e86abdff9f0739400eb625fb1ab5043e045082b0
Xie Yuheng <xyheme@gmail.com>
2026-06-03 05:11:21 +0800

    watch-mode: highlight file:line:col locations, RET/M-n/M-p navigation, <f5> rerun
    - Highlight file:line:col patterns with link face, -- message in error face
    - RET: jump to location in other window via find-file-other-window
    - M-n/M-p: navigate between locations in watch buffer (wrap-around)
    - <f5>: re-run command (replaces g)
    - q: disabled (use kill-buffer to close)
    - Fix watch--collect-locs to skip full property region (not just 1 char)

--- editors/emacs/watch-mode/watch-mode.el
+++ editors/emacs/watch-mode/watch-mode.el
@@ -30,6 +30,10 @@
   :type 'float
   :group 'watch)
 
+(defface watch-loc-face
+  '((t :inherit link))
+  "Face for file:line:col locations in watch output.")
+
 (defvar watch-command-history nil
   "History list for `watch' command input.")
 
@@ -117,7 +121,9 @@
                                                      'watch-fm-status t
                                                      'face status-face))))))
                          (goto-char (point-max))
-                         (insert output)
+                         (let ((start (point)))
+                           (insert output)
+                           (watch--highlight-locations start (point)))
                          (goto-char (point-max)))))))
                :file-handler t))))))
 
@@ -125,6 +131,90 @@
   "Insert KEY: VALUE line in the front matter."
   (insert (propertize (format "%s:" key) 'face 'font-lock-keyword-face))
   (insert (format " %s\n" value)))
+
+(defun watch--highlight-locations (beg end)
+  "Highlight file:line:col patterns between BEG and END."
+  (save-excursion
+    (goto-char beg)
+    (while (re-search-forward
+            "\\([^ \t\n:]+\\):\\([0-9]+\\):\\([0-9]+\\)\\( -- .*\\)?" end t)
+      (let* ((file (match-string 1))
+             (line (match-string 2))
+             (col (match-string 3))
+             (col-end (match-end 3)))
+        (add-text-properties
+         (match-beginning 0) col-end
+         `(face watch-loc-face
+           mouse-face highlight
+           help-echo ,(format "RET: jump to %s:%s:%s" file line col)
+           watch-loc t))
+        (when (match-string 4)
+          (let ((sep-beg (match-beginning 4)))
+            (add-text-properties sep-beg (+ sep-beg 4)
+                                 '(face font-lock-comment-face))
+            (add-text-properties (+ sep-beg 4) (match-end 4)
+                                 '(face error))))))))
+
+(defun watch--collect-locs ()
+  "Return sorted list of all watch-loc start positions in buffer."
+  (let ((locs nil)
+        (pos (point-min)))
+    (while (< pos (point-max))
+      (let ((next (text-property-any pos (point-max) 'watch-loc t)))
+        (if next
+            (let ((end (or (next-single-property-change next 'watch-loc)
+                           (point-max))))
+              (push next locs)
+              (setq pos end))
+          (setq pos (point-max)))))
+    (nreverse locs)))
+
+(defun watch--jump-to-loc-at (pos)
+  "Jump to the file:line:col at POS in other window."
+  (when (get-text-property pos 'watch-loc)
+    (let* ((end (or (next-single-property-change pos 'watch-loc)
+                    (point-max)))
+           (str (buffer-substring-no-properties pos end))
+           (parts (split-string str ":")))
+      (when (>= (length parts) 3)
+        (let* ((file (car parts))
+               (line (string-to-number (nth 1 parts)))
+               (col (string-to-number (nth 2 parts)))
+               (full (expand-file-name file watch--work-dir)))
+          (when (file-exists-p full)
+            (find-file-other-window full)
+            (goto-char (point-min))
+            (forward-line (- line 1))
+            (forward-char (- col 1))))))))
+
+(defun watch-jump-to-loc ()
+  "Jump to file:line:col on the current line in other window."
+  (interactive)
+  (let ((pos (save-excursion
+               (beginning-of-line)
+               (text-property-any (point) (line-end-position) 'watch-loc t))))
+    (when pos
+      (watch--jump-to-loc-at pos))))
+
+(defun watch-next-loc ()
+  "Move point to the next location in the buffer."
+  (interactive)
+  (let* ((locs (watch--collect-locs))
+         (next (or (cl-find-if (lambda (p) (> p (point))) locs)
+                   (car locs))))
+    (when next
+      (goto-char next)
+      (recenter))))
+
+(defun watch-prev-loc ()
+  "Move point to the previous location in the buffer."
+  (interactive)
+  (let* ((locs (watch--collect-locs))
+         (prev (or (cl-find-if (lambda (p) (< p (point))) locs :from-end t)
+                   (car (last locs)))))
+    (when prev
+      (goto-char prev)
+      (recenter))))
 
 (defun watch--on-change (event buf)
   "Handle file-notify EVENT for watch buffer BUF."
@@ -168,7 +258,11 @@
   (setq-local buffer-read-only t)
   (add-hook 'kill-buffer-hook #'watch--cleanup nil t))
 
-(define-key watch-mode-map (kbd "g") #'watch-rerun)
+(define-key watch-mode-map (kbd "<f5>") #'watch-rerun)
+(define-key watch-mode-map (kbd "RET") #'watch-jump-to-loc)
+(define-key watch-mode-map (kbd "M-n") #'watch-next-loc)
+(define-key watch-mode-map (kbd "M-p") #'watch-prev-loc)
+(define-key watch-mode-map (kbd "q") #'undefined)
 
 ;;;###autoload
 (defun watch (watch-dir command)
