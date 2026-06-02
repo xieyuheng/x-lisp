382f1204a7d11be0cebecaa1185960cb169cc264
Xie Yuheng <xyheme@gmail.com>
2026-06-03 06:35:02 +0800

    remove editors/ (meta-lisp-mode and warden-mode extracted as independent projects)

--- editors/emacs/meta-lisp-mode/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/.emacs.d/meta-lisp-mode
-cp * ~/.emacs.d/meta-lisp-mode
--- editors/emacs/warden-mode/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/.emacs.d/warden-mode
-cp *.el ~/.emacs.d/warden-mode
