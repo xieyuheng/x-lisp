01122e822a4c373247c186c8b5d7e415fe2fc6ce
Xie Yuheng <xyheme@gmail.com>
2026-06-06 02:43:02 +0800

    pass-dump use desugaredStmts for 060-desugar / 070-module-import fragment dumps
    Add packageDumpDesugaredFragments to dump fragment.desugaredStmts instead
    of fragment.stmts. Add prettyFragmentDesugaredStmts for Term-based stmts.
    Update 060-DesugarPass and 070-ModuleImportPass to use the new function.
    Sync meta-lisp.meta with corresponding .meta files.

--- packages/meta-lisp.js/src/meta/format/formatPrettyFragmentStmts.ts
+++ packages/meta-lisp.js/src/meta/format/formatPrettyFragmentStmts.ts
@@ -9,3 +9,12 @@ export function formatPrettyFragmentStmts(
     .map((node) => Ppml.formatNode(node, { width }))
     .join("\n\n")
 }
+
+export function formatPrettyFragmentDesugaredStmts(
+  width: number,
+  stmts: Array<M.Stmt<M.Term>>,
+): string {
+  return M.prettyFragmentDesugaredStmts(stmts)
+    .map((node) => Ppml.formatNode(node, { width }))
+    .join("\n\n")
+}
