4347ac293dcc910acd379705820fd7ceb237a7b8
Xie Yuheng <xyheme@gmail.com>
2026-06-05 21:09:42 +0800

    refactor: move CompilerOptions.ts into package/

--- packages/meta-lisp.js/src/meta/package/PackageConfig.ts
+++ packages/meta-lisp.js/src/meta/package/PackageConfig.ts
@@ -1,6 +1,6 @@
 import * as z from "zod"
 
-import type { CompilerOptions } from "../CompilerOptions.ts"
+import type { CompilerOptions } from "./CompilerOptions.ts"
 
 export type PackageConfig = {
   name: string
--- packages/meta-lisp.js/src/meta/package/index.ts
+++ packages/meta-lisp.js/src/meta/package/index.ts
@@ -1,4 +1,4 @@
-export * from "../CompilerOptions.ts"
+export * from "./CompilerOptions.ts"
 export * from "./loadPackage.ts"
 export * from "./loadPackageConfig.ts"
 export * from "./Package.ts"
