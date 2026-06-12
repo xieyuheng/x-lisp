c8a25de12e5578927e8c01ddb87b5af5e58c355a
Xie Yuheng <xyheme@gmail.com>
2026-06-12 17:38:05 +0800

    up

--- packages/meta-lisp.js/src/x86/passes/020-SubmitPass.ts
+++ packages/meta-lisp.js/src/x86/passes/020-SubmitPass.ts
@@ -2,81 +2,85 @@ import * as X86 from "../index.ts"
 
 export function SubmitPass(mod: X86.Mod, stmts: X86.Stmt[]): void {
   for (const stmt of stmts) {
-    switch (stmt.kind) {
-      case "DefineCodeStmt": {
-        X86.modDefine(
-          mod,
-          X86.CodeDefinition(stmt.name, stmt.blocks, stmt.location),
-        )
-        break
-      }
+    submitStmt(mod, stmt)
+  }
+}
 
-      case "DefineDataStmt": {
-        X86.modDefine(
-          mod,
-          X86.DataDefinition(stmt.name, stmt.fields, stmt.location),
-        )
-        break
-      }
+function submitStmt(mod: X86.Mod, stmt: X86.Stmt): void {
+  switch (stmt.kind) {
+    case "DefineCodeStmt": {
+      X86.modDefine(
+        mod,
+        X86.CodeDefinition(stmt.name, stmt.blocks, stmt.location),
+      )
+      break
+    }
 
-      case "DefineMetadataStmt": {
-        X86.modDefine(
-          mod,
-          X86.MetadataDefinition(stmt.name, stmt.fields, stmt.location),
-        )
-        break
-      }
+    case "DefineDataStmt": {
+      X86.modDefine(
+        mod,
+        X86.DataDefinition(stmt.name, stmt.fields, stmt.location),
+      )
+      break
+    }
 
-      case "DefineStructStmt": {
-        const typeCtor: X86.TypeConstructor = {
-          mod,
-          name: stmt.name,
-          parameters: stmt.parameters,
-          size: (argTypes) => {
-            const structDefinition = mod.definitions.get(stmt.name)
-            if (
-              structDefinition === undefined ||
+    case "DefineMetadataStmt": {
+      X86.modDefine(
+        mod,
+        X86.MetadataDefinition(stmt.name, stmt.fields, stmt.location),
+      )
+      break
+    }
+
+    case "DefineStructStmt": {
+      const typeCtor: X86.TypeConstructor = {
+        mod,
+        name: stmt.name,
+        parameters: stmt.parameters,
+        size: (argTypes) => {
+          const structDefinition = mod.definitions.get(stmt.name)
+          if (
+            structDefinition === undefined ||
               structDefinition.kind !== "StructDefinition"
-            ) {
-              let message = `[SubmitPass] unknown struct: ${stmt.name}`
-              throw new Error(message)
-            }
-            const env = X86.envPutMany(
-              X86.emptyEnv(),
-              stmt.parameters,
-              argTypes.map((t) => X86.TypeValue(t)),
-            )
-            let total = 0
-            for (const field of structDefinition.fields) {
-              total += X86.typeSize(X86.evaluateType(mod, env, field.exp))
-            }
-            return total
-          },
-        }
-        mod.definitions.set(
-          stmt.name,
-          X86.StructDefinition(stmt.name, typeCtor, stmt.fields, stmt.location),
-        )
-        break
+          ) {
+            let message = `[SubmitPass] unknown struct: ${stmt.name}`
+            throw new Error(message)
+          }
+          const env = X86.envPutMany(
+            X86.emptyEnv(),
+            stmt.parameters,
+            argTypes.map((t) => X86.TypeValue(t)),
+          )
+          let total = 0
+          for (const field of structDefinition.fields) {
+            total += X86.typeSize(X86.evaluateType(mod, env, field.exp))
+          }
+          return total
+        },
       }
+      mod.definitions.set(
+        stmt.name,
+        X86.StructDefinition(stmt.name, typeCtor, stmt.fields, stmt.location),
+      )
+      break
+    }
 
-      case "DefineSpaceStmt": {
-        X86.modDefine(
-          mod,
-          X86.SpaceDefinition(stmt.name, stmt.size, stmt.location),
-        )
-        break
-      }
+    case "DefineSpaceStmt": {
+      X86.modDefine(
+        mod,
+        X86.SpaceDefinition(stmt.name, stmt.size, stmt.location),
+      )
+      break
+    }
 
-      case "ClaimStmt": {
-        mod.claimedTypeExps.set(stmt.name, stmt.type)
-        break
-      }
+    case "ClaimStmt": {
+      mod.claimedTypeExps.set(stmt.name, stmt.type)
+      break
+    }
 
-      case "ClaimCodeMetadataStmt": {
-        mod.codeMetadataTypeExp = stmt.type
-        break
-      }
+    case "ClaimCodeMetadataStmt": {
+      mod.codeMetadataTypeExp = stmt.type
+      break
     }
   }
 }
