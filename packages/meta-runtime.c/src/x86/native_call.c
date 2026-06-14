aeb9a99bba78958f16c7612e8c419cbacc0f129a
Xie Yuheng <xyheme@gmail.com>
2026-06-15 02:15:48 +0800

    x86: fix stack alignment in native_call trampoline for even argc
    C entry rsp = 16n + 8. trampoline pushes rbx + r12 (16 bytes) + argc args.
    When argc is even, total pushes = even -> rsp 8-mod-16 at callq,
    causing SIGSEGV when callee uses AVX2 SIMD (e.g. strlen_avx2, vsnprintf).
    
    Fix: sub  before arg pushes when argc is even, add  back on cleanup.
    Also add missing clobbers: r8, r10, r11, cc.

--- packages/meta-runtime.c/src/x86/native_call.c
+++ packages/meta-runtime.c/src/x86/native_call.c
@@ -13,22 +13,30 @@ value_t native_call_native_fn(void *entry, uint8_t argc, value_t *args) {
     "movq %[arg_count], %%rbx\n\t"
     "movq %[args_ptr], %%rcx\n\t"
     "movq %%rbx, %%r9\n\t"
+    "testb $1, %%bl\n\t"
+    "jnz 2f\n\t"
+    "subq $8, %%rsp\n\t"
+    "2:\n\t"
     "decq %%r9\n\t"
-    "js 2f\n\t"
+    "js 3f\n\t"
     "1:\n\t"
     "pushq (%%rcx, %%r9, 8)\n\t"
     "decq %%r9\n\t"
     "jns 1b\n\t"
-    "2:\n\t"
+    "3:\n\t"
     "callq *%%r12\n\t"
     "leaq (%%rsp, %%rbx, 8), %%rsp\n\t"
+    "testb $1, %%bl\n\t"
+    "jnz 4f\n\t"
+    "addq $8, %%rsp\n\t"
+    "4:\n\t"
     "popq %%r12\n\t"
     "popq %%rbx\n\t"
     : "=a" (result)
     : [entry_addr] "D" (entry_addr),
       [arg_count] "S" (arg_count),
       [args_ptr] "d" (args_ptr)
-    : "rcx", "r9", "memory"
+    : "rcx", "r8", "r9", "r10", "r11", "cc", "memory"
   );
 
   return (value_t) result;
