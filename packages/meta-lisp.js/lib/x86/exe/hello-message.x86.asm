d81f0596f6e412c0dd4148c826e8b80a4de00005
Xie Yuheng <xyheme@gmail.com>
2026-07-15 03:13:29 +0800

    up

---
+++ packages/meta-lisp.js/lib/x86/exe/hello-message.x86.asm
@@ -0,0 +1,12 @@
+(define-data message "hello message\n")
+
+(define-code main
+  (block entry
+    (mov (reg rax) 1)
+    (mov (reg rdi) 1)
+    (mov (reg rsi) (address message))
+    (mov (reg rsi) (reg-deref (reg rsi)))
+    (mov (reg rdx) 14)
+    (syscall)
+    (mov (reg rax) 0)
+    (ret)))
--- packages/meta-lisp.js/lib/x86/exe/hello-world.x86.asm
+++ packages/meta-lisp.js/lib/x86/exe/hello-world.x86.asm
@@ -1,10 +1,8 @@
-(define-data message "hello world\n")
-
 (define-code main
   (block entry
     (mov (reg rax) 1)
-    (mov (reg rdx) 1)
-    (lea (reg rsi) (address message))
+    (mov (reg rdi) 1)
+    (mov (reg rsi) "hello world\n")
     (mov (reg rdx) 12)
     (syscall)
     (mov (reg rax) 0)
