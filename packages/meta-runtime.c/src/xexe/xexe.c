b54a9f3154c60779ff8f2132d108e630f07f6654
Xie Yuheng <xyheme@gmail.com>
2026-07-19 00:54:55 +0800

    [meta-runtime.c] `xexe_check`

--- packages/meta-runtime.c/src/xexe/xexe.c
+++ packages/meta-runtime.c/src/xexe/xexe.c
@@ -12,6 +12,13 @@ void xexe_free(xexe_t *self) {
   buffer_free(self->buffer);
 }
 
+void xexe_check(xexe_t *self) {
+  uint8_t magic[8] = {'x', 'e', 'x', 'e', 0, 0, 0, 0};
+  uint8_t machine[8] = {'x', '8', '6', '-', '6', '4', 0, 0};
+  assert(memcmp(self->header->magic, magic, 8) == 0);
+  assert(memcmp(self->header->machine, machine, 8) == 0);
+}
+
 uint64_t xexe_version(xexe_t *xexe) {
   return xexe->header->version;
 }
