c316d2bed3782d3c6aac3e1be07fad82ff3dda6e
Xie Yuheng <xyheme@gmail.com>
2026-07-20 19:38:35 +0800

    [meta-lisp.js] 190-SelectInstructionPass -- `selectDefinition` -- setup

--- packages/meta-lisp.js/src/meta/passes/190-SelectInstructionPass.ts
+++ packages/meta-lisp.js/src/meta/passes/190-SelectInstructionPass.ts
@@ -5,10 +5,39 @@ import * as X86 from "../../x86/index.ts"
 
 // translate basic-lisp to assembly-lisp (with variables)
 
-export function  SelectInstructionPass(pkg: M.Package, basicMod: B.Mod): X86.Mod {
+export function SelectInstructionPass(pkg: M.Package, basicMod: B.Mod): X86.Mod {
   const x86Mod = X86.createMod()
-
-  // TODO
-
+  const stmts = Array.from(basicMod.definitions.values())
+    .flatMap(definition => selectDefinition(definition))
+  X86.BuildPipeline(x86Mod, stmts)
   return x86Mod
+}
+
+export function selectDefinition(definition: B.Definition): Array<X86.Stmt> {
+  switch (definition.kind) {
+    case  "StructDefinition": {
+      // TODO
+      return []
+    }
+
+    case  "FunctionDefinition": {
+      // TODO
+      return []
+    }
+
+    case  "VariableDefinition": {
+      // TODO
+      return []
+    }
+
+    case  "ExternFunctionDefinition": {
+      // TODO
+      return []
+    }
+
+    case  "ExternVariableDefinition": {
+      // TODO
+      return []
+    }
+  }
 }
