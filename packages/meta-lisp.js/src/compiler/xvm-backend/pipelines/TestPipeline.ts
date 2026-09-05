917fe037f8e669ffdb1dc6031705118b1ab048fe
Xie Yuheng <xyheme@gmail.com>
2026-09-06 00:44:55 +0800

    format code

--- packages/meta-lisp.js/src/compiler/xvm-backend/pipelines/TestPipeline.ts
+++ packages/meta-lisp.js/src/compiler/xvm-backend/pipelines/TestPipeline.ts
@@ -6,9 +6,6 @@ import * as M from "../../../meta/index.ts"
 export function TestPipeline(pkg: M.Package): void {
   const currentDir = Path.dirname(fileURLToPath(import.meta.url))
   const xvmPath = Path.join(currentDir, "../../../../../xvm.c/src/xvm.exe")
-  const xvmExePath = Path.join(
-    M.packageOutputDirectory(pkg),
-    "bundle.xvm.exe",
-  )
+  const xvmExePath = Path.join(M.packageOutputDirectory(pkg), "bundle.xvm.exe")
   systemShellRun(xvmPath, ["test", xvmExePath])
 }
